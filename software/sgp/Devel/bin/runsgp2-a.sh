e66de4e9de840445ae62052781039e4575674a8b
Josep Francesc Abril <jabril@ub.edu>
2000-10-16 18:02:03 +0000

    runsgp2-a.sh now included in the noweb main document. human3iso.param updated to last version used by rguigo. Latest results using that parameter file, were:
     512  474  402  0.95  0.97  0.96  0.96  0.79  0.85  0.82  0.08  0.01

--- software/sgp/Devel/bin/runsgp2-a.sh
+++ software/sgp/Devel/bin/runsgp2-a.sh
@@ -1,12 +1,18 @@
-ISEQ=$1
-ODIR=$2
+#
+# $Id: runsgp2-a.sh,v 1.3 2000-10-16 18:02:03 jabril Exp $
+#
 
-IFILE=$3
+ISEQ=$1;
+ODIR=$2;
 
-SGP2=$SGP/sgp2.pl
-while read gene locus1 locus2
+IFILE=$3;
+
+SGP2=$SGP/bin/sgp2.pl;
+while read gene locus1 locus2;
 do
-      echo $gene $locus1 $locus2
-      $SGP2 -v -1 $ISEQ/$locus1 -2 $ISEQ/$locus2 -k $IFILE/ > $ODIR/$gene
-done
+    echo $gene $locus1 $locus2;
+    $SGP2 -v -1 "$ISEQ/$locus1" -2 "$ISEQ/$locus2"\
+          -k "$IFILE/$gene." -p "$IFILE/$gene." > "$ODIR/$gene.sgp";
+done;
 
+exit 0;
