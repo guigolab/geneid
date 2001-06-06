f21d259db1834ff3c771d76cbdf065a48efde5fa
Josep Francesc Abril <jabril@ub.edu>
2001-06-06 19:00:58 +0000

    + Added directories   -> contrib   : for the complementary programs that SGP requires   -> bin/param : for the parameter files that those programs shall need + Drafted function for parsing custom files. + Processing of command-line options half recycled.

--- software/sggp/bin/sgp.pl
+++ software/sggp/bin/sgp.pl
@@ -1,23 +1,19 @@
 #!/usr/bin/perl -w
 # This is perl, version 5.005_03 built for i386-linux
 #
-#line 450 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
-# $Id: sgp.pl,v 1.1.1.1 2001-06-02 11:24:16 jabril Exp $
-#line 466 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
+# $Id: sgp.pl,v 1.2 2001-06-06 19:00:58 jabril Exp $
 #
 use strict;
 #
-#line 249 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
 ######################################################################
 #                               SGP                                  #
 ######################################################################
 #
 #     Sinteny based Gene Prediction tool.
 #
-#     Copyright (C) 2000 - Josep Francesc ABRIL FERRANDO
+#     Copyright (C) 2001 - Josep Francesc ABRIL FERRANDO
 #                                   Genis PARRA FARRE
 #                                 Roderic GUIGO SERRA
-#line 426 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -36,33 +32,198 @@ use strict;
 ######################################################################
 #
 
-#line 227 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
+#### MODULES ####
+
+use Benchmark;
+  my @Timer = (new Benchmark);
+use Data::Dumper;
+local $Data::Dumper::Purity = 0;
+local $Data::Dumper::Deepcopy = 1; 
+
 #### CONSTANTS DEFINITION ####
 
+my $PROGRAM = "SGP";
+my @tmp_ver = split / +/, 
+    ': $Id: sgp.pl,v 1.2 2001-06-06 19:00:58 jabril Exp $ :';
+my $VERSION = "v$tmp_ver[3] [@tmp_ver[4,5,7]]";
+my ($T,$F) = (1,0); # for 'T'rue and 'F'alse
+my $line = ("#" x 60)."\n";
+my ($s,$r) = ("###","###\n");
+my $Error = "\<\<\<  ERROR  \>\>\> ";
+my $Warn  = "\<\<\< WARNING \>\>\> ";
+my $spw   = "\<\<\<         \>\>\> ";
+my $spl   = "\<\<\<\-\-\-\-\-\-\-\-\-\>\>\>\n";
 
-#### VARIABLES DECLARATION ####
+#### GLOBAL VARIABLES ####
 
+my $DATE = localtime;
+my $USER = $ENV{USER};
+#
+# Error/Warning Message List
+my %ErrorList = (
+                 
+                 UNKNOWN_CL_OPTION => "!!! Error trapped while processing command-line:\n",
+                 CMD_LINE_ERROR => "!!! Please, check your command-line options!!!\n",
+                 );
+#
+# Verbose Message List
+my %MessageList = (
+                   
+                   HEADER => $line."$s    RUNNING: $PROGRAM\n".
+                                   "$s       USER: $USER\n".
+                                   "$s       DATE: $DATE\n".$line,
+                   JOB_DONE => $line."$s  TOTAL TIME SPENT --> \%s\n".$line,
+                   );
 
 #### MAIN LOOP ####
 
-  
-#line 264 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
-exit(0);
+    &report('HEADER');
+    # get options
+    &Which_Options();
+    #
+    &report('JOB_DONE',&timing($T));
+    exit(0);
 
-#line 237 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
 ##### SUBS #####
 
+# Parsing command-line options and processing its parameters.
+# Checking input sequence files
+sub exists_file() {
+    my @files = @_;
+    my ($n, $r) = (' ', 0);
+    foreach $n (@files) {
+        $r++ if (-e "$n");
+    };
+    return $r;
+}
+sub check_fasta_format() {
+    my $file = $_[0];
+    my ($n, $c) = (undef, 0);
+    open(TMP,"< $file");
+    while (<TMP>) {
+        next unless /^>/;
+        />(\S+)\b/ && do {
+            $n = $1;
+            $c++;
+            next unless $c>1;
+        };
+        &go_to_die("FATAL ERROR !!! Multiple locus names found.\n  File \'$file\' must contain only one sequence definition.\n");
+    }
+    &go_to_die("FATAL ERROR !!! There is no '>' line, locus name not found.\n  Please, verify your fasta file \'$file\'\n") unless defined($n);
+    return $n;
+}
+sub Which_Options() {
+    $SIG{__WARN__} = sub { &warn('UNKNOWN_CL_OPTION',$_[0]) };
+    GetOptions( 
+                "1=s"      => \$Seq1         , # seqfile_1
+                "2=s"      => \$Seq2         , # seqfile_2
+                "g=s"      => \$geneid_opt   , # geneid options      
+                "P=s"      => \$geneid_param , # geneid parameter file 
+                "o=s"      => \$blast_opt    , # tblastx options 
+                "c=f"      => \$score_cutoff , # tblastx score cutoff
+                "s=f"      => \$shrink       , # shrink hsp's by
+                "t=s"      => \$tbx          , # read tblastx from file
+                "f=s"      => \$hsp          , # read HSP files in directory
+                "k=s"      => \$ofn          , # intermediate filename
+                "p=s"      => \$ps_output    , # postscript output 
+                "v"        => \$verbose_flg  , # verbose    
+                "h|help|?" => \$help_flg     , # print help
+                ) || (&warn('CMD_LINE_ERROR'), exit(1));
+    $SIG{__WARN__} = 'DEFAULT';
+    &prt_Help if $help_flg;
+    &prt_Header("Processsing Command-Line Options") if $verbose_flg;
+    # do seq files exists
+    my $file_number = &exists_file($Seq1, $Seq2);
+    &go_to_die("FATAL ERROR!!! Two sequences are needed (-1 and -2 options are mandatory).\n")
+        unless $file_number == 2; 
+    # extract basenames
+    $Seq1_Name = basename($Seq1);
+    $Seq2_Name = basename($Seq2);
+    # check if files are provided in fasta format and get locus names
+    $Loc1 = &check_fasta_format($Seq1);
+    $Loc2 = &check_fasta_format($Seq2);
+    &go_to_die("FATAL ERROR!!! Locus1($Loc1) have the same name as Locus2($Loc2).\n  Sequences \'$Seq1_Name\' and \'$Seq2_Name\' must have different locus names.\n")
+        if $Loc1 eq $Loc2;
+    # tmpfiles prefix
+    $SGPtmp1 = "$SGPTMP.$Seq1_Name";
+    $SGPtmp2 = "$SGPTMP.$Seq2_Name";
+    $SGPtmpG = "$SGPTMP.${Loc1}_${Loc2}"; 
+    do {
+        &copy_files("$hsp${Loc1}_${Loc2}.srQ", "$SGPtmpG.srQ");
+        &copy_files("$hsp${Loc1}_${Loc2}.srS", "$SGPtmpG.srS");
+        &copy_files("$hsp${Loc1}_${Loc2}.aln", "$SGPtmpG.aln");
+        $hsps_flg = 0;
+    } if defined($hsp);
+    do {
+        &copy_files("$tbx", "$SGPTMP.tbx");
+        $blast_flg = 0;
+    } if defined($tbx);
+    # $geneid_flg = 0;
+    $savefiles_flg = 1 if defined($ofn);
+    $plots_flg = 0 unless defined($ps_output);
+    # setting other variables
+    $GeneidOptions = "$geneid_opt"   if defined($geneid_opt);
+    $GeneidParam   = "$geneid_param" if defined($geneid_param);
+    $BlastOptions  = "$blast_opt"    if defined($blast_opt);
+    $verbose_str = "" if $verbose_flg;
+} # sub Which_Options
+
+$start = new Benchmark;
+$sys_rpt = system("$cmdline");
+$end = new Benchmark;
+&check_sys_result($sys_rpt);
+$ttime = timestr(timediff($end,$start));
+print STDERR "         Time Spent: $ttime \n";
 
 
 
+#
+sub check_sys_result() {
+    my $runOK = $F;
+    my $prog_exit = 0xffff & $_[0];
+    printf STDERR "### Command returned %#04x : ", $prog_exit;
+    if ($prog_exit == 0) {
+        print STDERR "ran with normal exit ...\n";
+        $runOK = $T;
+    } elsif ($prog_exit == 0xff00) {
+        print STDERR "command failed: $! ...\n";
+    } elsif (($prog_exit & 0xff) == 00) {
+        $prog_exit >>= 8;
+        print STDERR "ran with non-zero exit status $prog_exit ...\n";
+    } else {
+        print STDERR "ran with ";
+        if ($prog_exit &   0x80) {
+            $prog_exit &= ~0x80;
+            print STDERR "coredump from ";
+        };
+        print STDERR "signal $prog_exit ...\n";
+    };
+    return $runOK;
+} # check_sys_result()
+#
+sub max() {
+    my $z = shift @_;
+    foreach my $l (@_) { $z = $l if $l > $z };
+    return $z;
+} # max
+sub min() {
+    my $z = shift @_;
+    foreach my $l (@_) { $z = $l if $l < $z };
+    return $z;
+} # min
+#
+sub fill_right() { $_[0].($_[2] x ($_[1] - length($_[0]))) }
+sub fill_left()  { ($_[2] x ($_[1] - length($_[0]))).$_[0] }
+sub fill_mid()   { 
+    my $l = length($_[0]);
+    my $k = int(($_[1] - $l)/2);
+    ($_[2] x $k).$_[0].($_[2] x ($_[1] - ($l+$k)));
+} # fill_mid
+sub report() { print STDERR sprintf($MessageList{ shift @_ },@_) }
+sub warn() { print STDERR sprintf($ErrorList{ shift @_ }, @_) }
 
-
-
-
-#line 241 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
 #### EOF ####
 
-#line 320 "/home/ug/jabril/development/sggp/SintenicGenePrediction.nw"
 __DATA__
 
 =head1 NAME
