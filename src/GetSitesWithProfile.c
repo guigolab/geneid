8d6dc23010b09b66bcec496b64d83ea1d662527b
Enrique Blanco <enrique.blanco@crg.eu>
2001-12-18 15:37:20 +0000

    geneid v.1.1. final version:
    partial signals are detected as well as signals containing N's

--- src/GetSitesWithProfile.c
+++ src/GetSitesWithProfile.c
@@ -2,11 +2,11 @@
 *                                                                        *
 *   Module: GetSitesWithProfile                                          *
 *                                                                        *
-*   Splice sites prediction using Position Frequency Matrices            *
+*   Signal prediction by using a Position Weighted Array                 *
 *                                                                        *
-*   This file is part of the geneid Distribution                         *
+*   This file is part of the geneid 1.1 distribution                     *
 *                                                                        *
-*     Copyright (C) 2000 - Enrique BLANCO GARCIA                         *
+*     Copyright (C) 2001 - Enrique BLANCO GARCIA                         *
 *                          Roderic GUIGO SERRA                           * 
 *                                                                        *
 *  This program is free software; you can redistribute it and/or modify  *
@@ -24,117 +24,152 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/
 
-/*  $Id: GetSitesWithProfile.c,v 1.2 2000-08-08 14:17:32 eblanco Exp $  */
+/*  $Id: GetSitesWithProfile.c,v 1.3 2001-12-18 15:37:20 eblanco Exp $  */
 
 #include "geneid.h"
 
+/* Function TRANS: char -> integer such that A=0, C=1, G=2 and T/U=2 */
 extern int TRANS[];
+
+/* Maximum allowed number of generic sites */
 extern long NUMSITES;
 
-long  GetSitesWithProfile(char *s, profile *p, 
-			  site *st, 
-			  long l1, 
-			  long l2) 
+long  GetSitesWithProfile(char* s,
+                          profile* p,
+                          site* st, 
+                          long l1, 
+                          long l2) 
 {
-  int i;
+  int i,j;
   double score;
-  long ns=0,
-       is=0;
-  long left, 
-       right;
+  long ns,is;
+  long left,right;
   int index;
   
+  /* Final number of predicted signals (that type) */
+  ns = 0;
+  
+  /* 1. Searching sites between beginning of the sequence and p->offset */
+  if (!l1)
+    {
+      for (is = 0; is < p->offset && (ns<NUMSITES); is++)
+		{
+		  score=0.0;
+		  /* Applying part of the profile */
+		  for (i=p->offset-is, j=0; i < p->dimension; i++,j++) 
+			{
+			  /* i is the position inside the region */
+			  index = OligoToInt(s+j, p->order+1,5);
+			  
+			  if (index >= p->dimensionTrans)
+				score = score + -INFI;
+			  else
+				score = score + p->transitionValues[i][index];
+			}
+        
+		  if (score >= p->cutoff) 
+			{
+			  st[ns].Position = is + p->order;
+			  st[ns].Score=score;
+			  ns++;
+			}
+		}
+    }
+  
+  /* 2. Normal processing: predicting using the whole profile */
   /* left and right are the true boundaries of prediction */
   left  = MAX(0+p->order, l1 - p->offset);
   right = l2 - p->offset;
   s += left;
-
-  /* using Markov chain with order 0 */
-  if (p->order==0)
+  is = 0;     
+  /* Case A: Using Markov chain with order 0: PWM */
+  if (p->order == 0)
     {
       /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right- left + 1) && (ns<NUMSITES))
-	{ 
-	  /* is = 0..right */
-	  score=0.0;
-	  for (i=0;i<p->dimension;i++)
-	    {
-	      /* i is the position inside the region */
-	      index = TRANS[(int)(*(s + i))];
-	      if (index >= p->dimensionTrans)
-		score = score + -INFI;
-	      else
-		score = score + p->transitionValues[i][index];
-	    }
-	  
-	  if (score >= p->cutoff) 
-	    {
-	      st[ns].Position = left + is + p->offset;
-	      st[ns].Score=score;
-	      ns++;
-	    }
-	  is++;
-	  s++;
-	}
+		{ 
+		  /* is = 0..right */
+		  score=0.0;
+		  for (i=0;i<p->dimension;i++)
+			{
+			  /* i is the position inside the region */
+			  index = TRANS[(int)(*(s + i))];
+			  if (index >= p->dimensionTrans)
+				score = score + -INFI;
+			  else
+				score = score + p->transitionValues[i][index];
+			}
+		  
+		  if (score >= p->cutoff) 
+			{
+			  st[ns].Position = left + is + p->offset;
+			  st[ns].Score=score;
+			  ns++;
+			}
+		  is++;
+		  s++;
+		}
     }
-  /* using Markov chain with order 1 */
-  else if (p->order==1)
+  /* case B: Using Markov chain with order 1: dinucleotides */
+  else if (p->order == 1)
     {
       /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right- left + 1) && (ns<NUMSITES))
-	{ 
-	  /* is = 0..right */
-	  score=0.0;
-	  for (i=0;i<p->dimension;i++)
-	    {
-	      /* i is the position inside the region */
-	      index = 4*TRANS[(int)(*(s + i -1))] + TRANS[(int)(*(s + i))];
-	      if (index >= p->dimensionTrans)
-		score = score + -INFI;
-	      else
-		score = score + p->transitionValues[i][index];
-	    }
-	  
-	  if (score >= p->cutoff) 
-	    {
-	      st[ns].Position = left + is + p->offset;
-	      st[ns].Score=score;
-	      ns++;
-	    }
-	  is++;
-	  s++;
-	}
+		{ 
+		  /* is = 0..right */
+		  score=0.0;
+		  for (i=0;i<p->dimension;i++)
+			{
+			  /* i is the position inside the region */
+			  index = 5*TRANS[(int)(*(s + i -1))] + TRANS[(int)(*(s + i))];
+			  if (index >= p->dimensionTrans)
+				score = score + -INFI;
+			  else
+				score = score + p->transitionValues[i][index];
+			}
+		  
+		  if (score >= p->cutoff) 
+			{
+			  st[ns].Position = left + is + p->offset;
+			  st[ns].Score=score;
+			  ns++;
+			}
+		  is++;
+		  s++;
+		}
     }
+  /* case C: Using Markov chain with order > 1 */
   else
     {
       /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right- left + 1) && (ns<NUMSITES))
-	{ 
-	  /* is = 0..right */
-	  score=0.0;
-	  for (i=0;i<p->dimension;i++)
-	    {
-	      /* i is the position inside the region */
-	      index = OligoToInt(s + i - p->order , p->order+1);
-	      if (index >= p->dimensionTrans)
-		score = score + -INFI;
-	      else
-		score = score + p->transitionValues[i][index];
-	    }
-	  
-	  if (score >= p->cutoff) 
-	    {
-	      st[ns].Position = left + is + p->offset;
-	      st[ns].Score=score;
-	      ns++;
-	    }
-	  is++;
-	  s++;
-	}
+		{ 
+		  /* is = 0..right */
+		  score=0.0;
+		  for (i=0;i<p->dimension;i++)
+			{
+			  /* i is the position inside the region */
+			  index = OligoToInt(s + i - p->order , p->order+1,5);
+			  if (index >= p->dimensionTrans)
+				score = score + -INFI;
+			  else
+				score = score + p->transitionValues[i][index];
+			}
+		  
+		  if (score >= p->cutoff) 
+			{
+			  st[ns].Position = left + is + p->offset;
+			  st[ns].Score=score;
+			  ns++;
+			}
+		  is++;
+		  s++;
+		}
     }
   
-  if (ns==NUMSITES)
-    printError("Too many predicted sites: Change NUMSITES parameter"); 
+  if (ns >= NUMSITES)
+    printError("Too many predicted sites: increase NUMSITES parameter");
+  
   return(ns);
 }
 
