69dbae206c6b5dd5ce77c133346d4eebf690551c
Enrique Blanco <enrique.blanco@crg.eu>
2001-12-18 15:02:24 +0000

    geneid v1.1. final version:
    INPUT the sequence to avoid building STOP codons between 2 exons

--- src/BuildInitialExons.c
+++ src/BuildInitialExons.c
@@ -2,11 +2,11 @@
 *                                                                        *
 *   Module: BuildInitialExons                                            *
 *                                                                        *
-*   Using lists of splice sites, it builds initial exons                 *
+*   From start/stop codons and donor sites, to build initial exons       *
 *                                                                        *
-*   This file is part of the geneid Distribution                         *
+*   This file is part of the geneid 1.1 distribution                     *
 *                                                                        *
-*     Copyright (C) 2000 - Enrique BLANCO GARCIA                         *
+*     Copyright (C) 2001 - Enrique BLANCO GARCIA                         *
 *                          Roderic GUIGO SERRA                           * 
 *                                                                        *
 *  This program is free software; you can redistribute it and/or modify  *
@@ -24,107 +24,144 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/
 
-/*  $Id: BuildInitialExons.c,v 1.2 2000-08-08 14:15:10 eblanco Exp $  */
+/*  $Id: BuildInitialExons.c,v 1.3 2001-12-18 15:02:24 eblanco Exp $  */
 
 #include "geneid.h"
 
+/* Maximum allowed number of generic exons (divided by RFIRST) */
 extern long NUMEXONS;
+extern long MAXBACKUPSITES;
 
 long BuildInitialExons(site *Start, long nStarts, 
-		       site *Donor, long nDonors,
-		       site *Stop, long nStops,
-		       int MaxDonors,
-		       exonGFF *Exon) 
+                       site *Donor, long nDonors,
+                       site *Stop, long nStops,
+                       int MaxDonors,
+					   char* Sequence,
+                       exonGFF *Exon) 
 {
+  /* Best exons built by using the current start codon */
   exonGFF *LocalExon;
   int nLocalExons, LowestLocalExon;
   double LowestLocalScore;
+  
+  /* Maximum allowed number of predicted initial exons per fragment */
+  long HowMany;
+  
   int Frame;
-  long i, j=0, js, k=0, ks;
+  long i, j, js, k, ks;
   int l;
+  
+  /* Final number of predicted initial exons */
   long nExon;
   
-  LocalExon = (exonGFF*) calloc(MaxDonors, sizeof(exonGFF));
   
-  /* main loop, for each Start codon */
-  for (i = 0, nExon = 0;
-       (nExon<(int)(NUMEXONS/RFIRST)) && (i < nStarts); 
-       i++)
-  { 
-    Frame = (Start+i)->Position % 3;
-    nLocalExons = 0;
-    LowestLocalScore = DBL_MAX;
-    
-    /* advance Stops to Start */
-    while (((Stop+j)->Position+1 < (Start+i)->Position) && (j < nStops))
-      j++;
-    js=j;
-    
-    /* find first Stop in Frame with Start */
-    while ((((Stop+js)->Position+1) % 3 != Frame) && (js < nStops))
-      js++;
-    
-    /* advance Donors to Start */
-    while (((Donor+k)->Position < (Start+i)->Position+2) && (k < nDonors))
-      k++;
-    ks=k;
-    
-    /* every Donor between Start and Stop[js] defines an initial exon */
-    /* keep only the top scoring MaxDonors */
+  /* Allocating space for exons built by using the current start codon */
+  /* MaxDonors is the maximum allowed number of exons with this signal */
+  if ((LocalExon = (exonGFF*) calloc(MaxDonors,sizeof(exonGFF))) == NULL)
+    printError("Not enough memory: local first exons"); 
+  
+  /* Main loop, forall start codon looking for donor sites... */
+  /* ...until the first stop codon in frame is reached */
+  HowMany = (MAXBACKUPSITES)? (long)(NUMEXONS/RFIRST): NUMEXONS;
+  for (i = 0, j = 0, k = 0, nExon = 0;
+       (nExon < HowMany) && (i < nStarts); i++)
+	{ 
+	  /* Reset the best local exons array */
+	  nLocalExons = 0;
+	  LowestLocalScore = DBL_MAX;
+	  
+	  /* Computing the frame to look for the first stop in frame */
+	  Frame = (Start+i)->Position % 3;
+	  
+	  /* Skip previous Stops to current Start */
+	  while (((Stop+j)->Position+1 < (Start+i)->Position) && (j < nStops))
+		j++;
 
-    while ((js == nStops || (Donor+ks)->Position < (Stop+js)->Position + 1 + 2) 
-	   && (ks < nDonors)) 
-      {
-	if (nLocalExons < MaxDonors) 
-	  {
-	    (LocalExon+nLocalExons)->Acceptor=(Start+i);
-	    (LocalExon+nLocalExons)->Donor=(Donor+ks);
-	    if ((Donor+ks)->Score < LowestLocalScore) 
-	      {
-		LowestLocalScore = (Donor+ks)->Score;
-		LowestLocalExon = nLocalExons;
-	      }
-	    nLocalExons++;
-	  }
-	else 
-	  {
-	    if ((Donor+ks)->Score > LowestLocalScore) {
-	      for (l=LowestLocalExon;l<nLocalExons-1;l++)
-		LocalExon[l]=LocalExon[l+1];
-	    (LocalExon+nLocalExons-1)->Acceptor=(Start+i);
-	    (LocalExon+nLocalExons-1)->Donor=(Donor+ks);
-	    
-	    LowestLocalExon = 0;
-	    LowestLocalScore = (LocalExon+0)->Donor->Score;
-	    for (l=1;l<nLocalExons;l++)
-	      if ((LocalExon+l)->Donor->Score < LowestLocalScore) 
+	  /* Save counter j for the next iteration */
+	  js=j;
+    
+	  /* Finding first Stop in Frame with current Start */
+	  while ((((Stop+js)->Position+1) % 3 != Frame) && (js < nStops))
+		js++;
+	  
+	  /* Skip previous Donors to current Start */
+	  while (((Donor+k)->Position < (Start+i)->Position+2) && (k < nDonors))
+		k++;
+	  
+	  /* Save counter k for the next iteration */
+	  ks=k;
+	  
+	  /* a) Every Donor between Start and that Stop defines an initial exon */
+	  /* b) If not any Stop in frame after current Start: every donor is OK */
+	  while ((js == nStops || (Donor+ks)->Position < (Stop+js)->Position + 1 + 2)
+			 && (ks < nDonors)) 
 		{
-		  LowestLocalScore = (LocalExon+l)->Donor->Score;
-		  LowestLocalExon = l;
+		  /* a. There is room to save this new exon */
+		  if (nLocalExons < MaxDonors) 
+			{
+			  (LocalExon+nLocalExons)->Acceptor=(Start+i);
+			  (LocalExon+nLocalExons)->Donor=(Donor+ks);
+			  
+			  /* Updating the worst exon pointer */
+			  if ((Donor+ks)->Score < LowestLocalScore)
+				{
+				  LowestLocalScore = (Donor+ks)->Score;
+				  LowestLocalExon = nLocalExons;
+				}
+			  nLocalExons++;
+			}
+		  else 
+			{
+			  /* b. Keep only the top scoring MaxDonors */
+			  if ((Donor+ks)->Score > LowestLocalScore)
+				{
+				  /* Extract the worst exon and input the new exon */
+				  for (l=LowestLocalExon;l<nLocalExons-1;l++)
+					LocalExon[l]=LocalExon[l+1];
+				  
+				  (LocalExon+nLocalExons-1)->Acceptor=(Start+i);
+				  (LocalExon+nLocalExons-1)->Donor=(Donor+ks);
+				  
+				  /* Updating the worst exon pointer */
+				  LowestLocalExon = 0;
+				  LowestLocalScore = (LocalExon+0)->Donor->Score;
+				  for (l=1;l<nLocalExons;l++)
+					if ((LocalExon+l)->Donor->Score < LowestLocalScore) 
+					  {
+						LowestLocalScore = (LocalExon+l)->Donor->Score;
+						LowestLocalExon = l;
+					  }
+				} /* end if */
+			} /* end else */
+		  
+		  /* Next donor between current start and stop in frame */
+		  ks++;
+		} /* end while */
+	  
+	  /* Save the best exons beginning by the current start */
+	  for (l=0;(l<nLocalExons) && (nExon<HowMany);l++) 
+		{
+		  Exon[nExon] = LocalExon[l];
+		  (Exon+nExon)->Frame = 0;
+		  (Exon+nExon)->Remainder = ((Exon+nExon)->Donor->Position -
+									 (Exon+nExon)->Acceptor->Position + 1 ) % 3;
+		  (Exon+nExon)->Remainder = (3 - (Exon+nExon)->Remainder) % 3; 
+         strcpy((Exon+nExon)->Type,"First");
+         strcpy((Exon+nExon)->Group,NOGROUP);
+         (Exon+nExon)->evidence = 0;
+
+		 /* Store info about frame and remainder nucleotides to avoid building stops in frame */
+		 ComputeStopInfo((Exon+nExon),Sequence);
+
+         nExon++;
 		}
-	    }
-	  }
-	ks++;
-      }
-    
-    /* put LocalExon into InitialExons. Within Start sort by Donor */  
-    for (l=0;(l<nLocalExons) && (nExon<(int)(NUMEXONS/RFIRST));l++) 
-      {
-	Exon[nExon] = LocalExon[l];
-	(Exon+nExon)->Frame = 0;
-	(Exon+nExon)->Remainder = ((Exon+nExon)->Donor->Position -
-				   (Exon+nExon)->Acceptor->Position + 1 ) % 3;
-	(Exon+nExon)->Remainder = (3 - (Exon+nExon)->Remainder) % 3; 
-	strcpy((Exon+nExon)->Type,"First");
-	(Exon+nExon)->Group = NOGROUP;
-	(Exon+nExon)->evidence = 0;
-	nExon++;
-    }
-  }
-
-  if (nExon>=(int)(NUMEXONS/RFIRST))
-   printError("Too many predicted exons: Change RFIRST parameter");
-
+	}
+  
+  if (nExon >= HowMany)
+	printError("Too many initial exons: decrease RFIRST parameter");
+  
   free(LocalExon);
+  
   return(nExon);
 }
+
