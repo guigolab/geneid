5535050e4a18d3587b6e2b2ec5c646d4aa34cb2f
Enrique Blanco <enrique.blanco@crg.eu>
2001-12-18 15:38:38 +0000

    geneid v.1.1. final version:
    partial signals and signals containing N's are detected

--- software/geneid/src/GetStopCodons.c
+++ software/geneid/src/GetStopCodons.c
@@ -2,11 +2,11 @@
 *                                                                        *
 *   Module: GetStopCodons                                                *
 *                                                                        *
-*   Stop codons prediction using Position Frequency Matrices.            *
+*   Stop codons prediction by using Position Weighted arrays             *
 *                                                                        *
-*   This file is part of the geneid Distribution                         *
+*   This file is part of the geneid 1.1 distribution                     *
 *                                                                        *
-*     Copyright (C) 2000 - Enrique BLANCO GARCIA                         *
+*     Copyright (C) 2001 - Enrique BLANCO GARCIA                         *
 *                          Roderic GUIGO SERRA                           * 
 *                                                                        *
 *  This program is free software; you can redistribute it and/or modify  *
@@ -24,24 +24,28 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/
 
-/*  $Id: GetStopCodons.c,v 1.2 2000-08-08 14:17:54 eblanco Exp $  */
+/*  $Id: GetStopCodons.c,v 1.3 2001-12-18 15:38:38 eblanco Exp $  */
 
 #include "geneid.h"
 
+/* Function TRANS: char -> integer such that A=0, C=1, G=2 and T/U=2 */
 extern int TRANS[];
+
+/* Maximum allowed number of generic sites */
 extern long NUMSITES;
 
-long GetStopCodons(char *s, profile *p, 
-		   site *sc, 
-		   long l1, 
-		   long l2) 
+long GetStopCodons(char* s,
+                   profile* p,
+                   site* sc, 
+                   long l1, 
+                   long l2) 
 {
-  long ns=0,
-       is=0;
+  long ns,is;
   double score;
-  int i;
+  int i,j;
   
-  static char *stop[] = 
+  /* Strings defining Stop codons */
+  static char *stop[] =
   {
     "TAA",
     "TAG",
@@ -49,145 +53,183 @@ long GetStopCodons(char *s, profile *p,
   };
 
   char codon[LENGTHCODON];
-  long left, 
-       right;
+  long left,right;
   int index;
-
+  
+  /* Final number of predicted stops */
+  ns = 0;
+  
+  /* 1. Searching sites between beginning of the sequence and p->offset */
+  if (!l1)
+    {
+      for (is = 0; is < p->offset && (ns<NUMSITES); is++)
+		{
+		  score=0.0;
+		  /* Applying part of the profile */
+		  for (i=p->offset-is, j=0; i < p->dimension; i++,j++) 
+			{
+			  /* i is the position inside the region */
+			  index = OligoToInt(s+j, p->order+1,5);
+			  
+			  if (index >= p->dimensionTrans)
+				score = score + -INFI;
+			  else
+				score = score + p->transitionValues[i][index];
+			}
+		  
+		  if (score >= p->cutoff) 
+			{
+			  sc[ns].Position = is + p->order;
+			  sc[ns].Score=score;
+			  ns++;
+			}
+		}
+    }
+  
+  /* 2. Normal processing */
   /* left and right are the true boundaries of prediction */
   left  = MAX(0+p->order,l1 - p->offset);
   right = l2 - p->offset;
   s += left;
-
+  is = 0;     
+  
+  /* Case A: Using Markov chain with order 0: PWM */
   if (p->order==0)
     {
       /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right - left + 1) && (ns<NUMSITES))  
-	{
-	  strncpy(codon,s+p->offset+1,LENGTHCODON); 
-	  codon[LENGTHCODON]='\0';
-	  
-	  if (!strcmp(codon,stop[0]) ||
-	      !strcmp(codon,stop[1]) ||
-	      !strcmp(codon,stop[2])) 
-	    {
-	      score=0.0;
-	      for (i=0;i<p->dimension;i++) 
 		{
-		  /* i is the position inside the region */
-		  index = TRANS[(int)(*(s + i))];
-		  if (index >= p->dimensionTrans)
-		    score = score + -INFI;
-		  else
-		    score = score + p->transitionValues[i][index];
+		  /* The candidate region must contain the STOP in the right position */
+		  strncpy(codon,s+p->offset+1,LENGTHCODON);
+		  codon[LENGTHCODON]='\0';
+		  
+		  if (!strcmp(codon,stop[0]) ||
+			  !strcmp(codon,stop[1]) ||
+			  !strcmp(codon,stop[2])) 
+			{
+			  score=0.0;
+			  for (i=0;i<p->dimension;i++) 
+				{
+				  /* i is the position inside the region */
+				  index = TRANS[(int)(*(s + i))];
+				  if (index >= p->dimensionTrans)
+					score = score + -INFI;
+				  else
+					score = score + p->transitionValues[i][index];
+				}
+			  
+			  if (score >= p->cutoff) 
+				{
+				  /* Position given is last coding position before Stop */ 
+				  sc[ns].Position=left + is + p->offset;
+				  sc[ns].Score=score;
+				  ns++;
+				}
+			} 
+		  is++;
+		  s++;
 		}
-	      
-	      if (score >= p->cutoff) 
-		{
-		  /* Position given is last coding position before Stop */ 
-		  sc[ns].Position=left + is + p->offset;
-		  sc[ns].Score=score;
-		  ns++;
-		}
-	    } 
-	  is++;
-	  s++;
-	}
     }   
+  /* Case B: Using Markov chain with order 1: dinucleotides */
   else if (p->order==1)
     {
       /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right - left + 1) && (ns<NUMSITES))  
-	{
-	  strncpy(codon,s+p->offset+1,LENGTHCODON); 
-	  codon[LENGTHCODON]='\0';
-	  
-	  if (!strcmp(codon,stop[0]) ||
-	      !strcmp(codon,stop[1]) ||
-	      !strcmp(codon,stop[2])) 
-	    {
-	      score=0.0;
-	      for (i=0;i<p->dimension;i++) 
 		{
-		  /* i is the position inside the region */
-		  index = 4*TRANS[(int)(*(s + i -1))] + TRANS[(int)(*(s + i))];
-		  if (index >= p->dimensionTrans)
-		    score = score + -INFI;
-		  else
-		    score = score + p->transitionValues[i][index];
-		}
-	      
-	      if (score >= p->cutoff) 
-		{
-		  /* Position given is last coding position before Stop */ 
-		  sc[ns].Position=left + is + p->offset;
-		  sc[ns].Score=score;
-		  ns++;
-		}
-	    } 
-	  is++;
-	  s++;
-	}     
+		  /* The candidate region must contain the STOP in the right position */
+		  strncpy(codon,s+p->offset+1,LENGTHCODON);
+		  codon[LENGTHCODON]='\0';
+		  
+		  if (!strcmp(codon,stop[0]) ||
+			  !strcmp(codon,stop[1]) ||
+			  !strcmp(codon,stop[2])) 
+			{
+			  score=0.0;
+			  for (i=0;i<p->dimension;i++) 
+				{
+				  /* i is the position inside the region */
+				  index = 5*TRANS[(int)(*(s + i -1))] + TRANS[(int)(*(s + i))];
+				  if (index >= p->dimensionTrans)
+					score = score + -INFI;
+				  else
+					score = score + p->transitionValues[i][index];
+				}
+			  
+			  if (score >= p->cutoff) 
+				{
+				  /* Position given is last coding position before Stop */ 
+				  sc[ns].Position=left + is + p->offset;
+				  sc[ns].Score=score;
+				  ns++;
+				}
+			} 
+		  is++;
+		  s++;
+		}     
     }
+  /* case C: Using Markov chain with order > 1 */
   else
     {
-     /* discovering splice sites with current profile */
+	  /* discovering splice sites with current profile */
       while (*(s+p->dimension) && (is < right - left + 1) && (ns<NUMSITES))  
-	{
-	  strncpy(codon,s+p->offset+1,LENGTHCODON); 
-	  codon[LENGTHCODON]='\0';
-	  
-	  if (!strcmp(codon,stop[0]) ||
-	      !strcmp(codon,stop[1]) ||
-	      !strcmp(codon,stop[2])) 
-	    {
-	      score=0.0;
-	      for (i=0;i<p->dimension;i++) 
 		{
-		  /* i is the position inside the region */
-		  index = OligoToInt(s + i - p->order , p->order+1); 
-		  if (index >= p->dimensionTrans)
-		    score = score + -INFI;
-		  else
-		    score = score + p->transitionValues[i][index];
-		}
-	      
-	      if (score >= p->cutoff) 
-		{
-		  /* Position given is last coding position before Stop */ 
-		  sc[ns].Position=left + is + p->offset;
-		  sc[ns].Score=score;
-		  ns++;
-		}
-	    } 
-	  is++;
-	  s++;
-	} 
+		  /* The candidate region must contain the STOP in the right position */
+		  strncpy(codon,s+p->offset+1,LENGTHCODON);
+		  codon[LENGTHCODON]='\0';
+		  
+		  if (!strcmp(codon,stop[0]) ||
+			  !strcmp(codon,stop[1]) ||
+			  !strcmp(codon,stop[2])) 
+			{
+			  score=0.0;
+			  for (i=0;i<p->dimension;i++) 
+				{
+				  /* i is the position inside the region */
+				  index = OligoToInt(s + i - p->order , p->order+1,5); 
+				  if (index >= p->dimensionTrans)
+					score = score + -INFI;
+				  else
+					score = score + p->transitionValues[i][index];
+				}
+			  
+			  if (score >= p->cutoff) 
+				{
+				  /* Position given is last coding position before Stop */ 
+				  sc[ns].Position=left + is + p->offset;
+				  sc[ns].Score=score;
+				  ns++;
+				}
+			} 
+		  is++;
+		  s++;
+		} 
     }
-
-  /* Get remaining stops until the end of sequence. Set score to 0 for those */
+  
+  /* 3. Remaining stops until the end of sequence. Set score to 0 for those */
   if (!(s+p->dimension))
     {
       s=(s-is);
       is+=p->offset;
       
       while (*(s+is) && (ns<NUMSITES))  
-	{
-	  strncpy(codon,(s+is),3); 
-	  codon[3]='\0';
-	  if (!strcmp(codon,stop[0]) || !strcmp(codon,stop[1]) || 
-	      !strcmp(codon,stop[2])) 
-	    {
-	      /* Position given is last coding position before Stop */
-	      sc[ns].Position=is-1;  
-	      sc[ns].Score=0;
-	      ns++;
-	    } 
-	  is++;
-	}
+		{
+		  /* The candidate region must contain the STOP in the right position */
+		  strncpy(codon,(s+is),LENGTHCODON);
+		  codon[LENGTHCODON]='\0';
+		  
+		  if (!strcmp(codon,stop[0]) || !strcmp(codon,stop[1]) || !strcmp(codon,stop[2]))
+			{
+			  /* Position given is last coding position before Stop */
+			  sc[ns].Position=is-1;  
+			  sc[ns].Score=0;
+			  ns++;
+			} 
+		  is++;
+		}
     }
   
-  if (ns==NUMSITES)
-    printError("Too many predicted sites: Change NUMSITES parameter");
-
+  if (ns >= NUMSITES)
+    printError("Too many predicted sites: increase NUMSITES parameter");
+  
   return(ns);
 }
