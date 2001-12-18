24df9dd43dba6db9d72bdb4b9737ddda4cac92c2
Enrique Blanco <enrique.blanco@crg.eu>
2001-12-18 14:57:36 +0000

    geneid 1.1. Final version

--- software/geneid/src/BackupGenes.c
+++ software/geneid/src/BackupGenes.c
@@ -2,11 +2,11 @@
 *                                                                        *
 *   Module: BackupGenes                                                  *
 *                                                                        *
-*   Save genes that GenAmic will need at next split.                     *
+*   To save best partial genes between 2 contigous fragments             *
 *                                                                        *
-*   This file is part of the geneid Distribution                         *
+*   This file is part of the geneid 1.1 distribution                     *
 *                                                                        *
-*     Copyright (C) 2000 - Enrique BLANCO GARCIA                         *
+*     Copyright (C) 2001 - Enrique BLANCO GARCIA                         *
 *                          Roderic GUIGO SERRA                           * 
 *                                                                        *
 *  This program is free software; you can redistribute it and/or modify  *
@@ -24,12 +24,14 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/
 
-/*  $Id: BackupGenes.c,v 1.2 2000-07-26 08:25:19 eblanco Exp $  */
+/*  $Id: BackupGenes.c,v 1.3 2001-12-18 14:57:36 eblanco Exp $  */
 
 #include "geneid.h"
 
+/* Maximum allowed number of sites and exons to save */
 extern long MAXBACKUPSITES, MAXBACKUPEXONS;
 
+/* Increase counters modulus a long number */
 long IncrMod(long x, long Modulus)
 {
   long z;
@@ -37,25 +39,26 @@ long IncrMod(long x, long Modulus)
   z = x+1;
   if (z == Modulus)
     z = 0;
+  
   return(z);
 }
 
-/* It saves the information about a exon */
+/* Saving exon information (features) into the dumpster */
 exonGFF* backupExon(exonGFF* E, exonGFF* Prev, packDump* d)
 {
-  /* backup acceptor */
+  /* back-up acceptor */
   d->dumpSites[d->ndumpSites].Position = E->Acceptor->Position;
   d->dumpSites[d->ndumpSites].Score = E->Acceptor->Score;
   d->dumpExons[d->ndumpExons].Acceptor = &(d->dumpSites[d->ndumpSites]); 
   d->ndumpSites = IncrMod(d->ndumpSites, MAXBACKUPSITES);
   
-  /* backup donor */
+  /* back-up donor */
   d->dumpSites[d->ndumpSites].Position = E->Donor->Position;
   d->dumpSites[d->ndumpSites].Score = E->Donor->Score;
   d->dumpExons[d->ndumpExons].Donor = &(d->dumpSites[d->ndumpSites]); 
   d->ndumpSites = IncrMod(d->ndumpSites, MAXBACKUPSITES);
   
-  /* back-up exon */
+  /* back-up exon properties */
   strcpy(d->dumpExons[d->ndumpExons].Type, E->Type);
   d->dumpExons[d->ndumpExons].Frame  = E->Frame;
   d->dumpExons[d->ndumpExons].Strand = E->Strand; 
@@ -64,16 +67,19 @@ exonGFF* backupExon(exonGFF* E, exonGFF* Prev, packDump* d)
   d->dumpExons[d->ndumpExons].SRScore = E->SRScore;
   d->dumpExons[d->ndumpExons].GeneScore  = E->GeneScore;
   d->dumpExons[d->ndumpExons].Remainder = E->Remainder; 
-  d->dumpExons[d->ndumpExons].Group = E->Group;
+  strcpy(d->dumpExons[d->ndumpExons].Group,E->Group);
   d->dumpExons[d->ndumpExons].offset1 = E->offset1;
   d->dumpExons[d->ndumpExons].offset2 = E->offset2;
+  d->dumpExons[d->ndumpExons].lValue = E->lValue;
+  d->dumpExons[d->ndumpExons].rValue = E->rValue;
   d->dumpExons[d->ndumpExons].evidence = E->evidence;
   d->dumpExons[d->ndumpExons].PreviousExon = Prev;
   
+  /* Returns the new exon recently created */
   return(&(d->dumpExons[d->ndumpExons]));
 }
 
-/* It saves the information about a gene, saving all its exons */
+/* Saving all about a gene: exons, sites, properties */
 exonGFF* backupGene(exonGFF* E, packDump* d)
 {
   exonGFF* PrevExon;
@@ -83,25 +89,25 @@ exonGFF* backupGene(exonGFF* E, packDump* d)
   if (E->Strand == '*')
     ResExon = E; 
   else
-     {
-       /* Is this exon backuped before? */
-       ResExon  = (exonGFF*) getExonDumpHash(E, d->h);
-       
-       /* New exon: backup it and add it at hash table */
-       if (ResExon == NULL)
-	 {
-	   PrevExon = backupGene(E->PreviousExon,d);
-	   ResExon = backupExon(E,PrevExon,d);
-	   d->ndumpExons = IncrMod(d->ndumpExons, MAXBACKUPEXONS);
-	   /* adding this exon at hash table */
-	   setExonDumpHash(ResExon, d->h);       
-         }
-       /* if this exon exists, finish backup gene */
-     }
+	{
+	  /* Ckeckpoint to discover if exon is already in the dumpster */
+	  ResExon  = (exonGFF*) getExonDumpHash(E, d->h);
+	  
+	  /* New exon: save it and insert into the hash table */
+	  if (ResExon == NULL)
+		{
+		  PrevExon = backupGene(E->PreviousExon,d);
+		  ResExon = backupExon(E,PrevExon,d);
+		  d->ndumpExons = IncrMod(d->ndumpExons, MAXBACKUPEXONS);
+		  /* adding this exon at hash table */
+		  setExonDumpHash(ResExon, d->h);       
+		}
+	  /* if this exon exists, finish backup gene */
+	}
   return(ResExon);
 }
 
-/* It saves the information of partial genes */
+/* It saves the information about partial genes (packGenes) */
 void BackupGenes(packGenes* pg, int nclass, packDump* d)
 {
   int i,j;
@@ -115,7 +121,7 @@ void BackupGenes(packGenes* pg, int nclass, packDump* d)
   pg->GOptim = backupGene(pg->GOptim, d);
 }
 
-/* It saves the information of d-genes */
+/* It saves information about d-exons: exons needed the next iteration */
 void BackupArrayD(packGenes* pg, long accSearch,
                   gparam* gp, packDump* dumpster)
 {
@@ -127,83 +133,89 @@ void BackupArrayD(packGenes* pg, long accSearch,
   long nBackups=0;
   short remainder;
   char mess[MAXSTRING];
-
+  
+  /* Traversing sort-by-donor array to save some genes (assembling rules) */
+  /* These exons have to be beyond the point accSearch (preserve ordering) */
   for(i=0; i < gp->nclass ; i++)
     {
+      /* Get data from this assembling rule */
       j = pg->je[i];
       MaxDist = gp->Md[i];
       MinDist = gp->md[i];
       
-      /* First: update d-array until the boundary-exon acceptor */
+      /* 1. Update best genes using remaining exons before accSearch - md */
       while(j < pg->km[i] &&
-	    ((pg->d[i][j]->Donor->Position + pg->d[i][j]->offset2)
-	     <=
-	     accSearch - MinDist))
-	{
-	  remainder = pg->d[i][j]->Remainder;
-	  if (pg->d[i][j]->GeneScore > pg->Ga[i][remainder]->GeneScore)
-	    pg->Ga[i][remainder] = pg->d[i][j];
-	  j++;
-	}
+			((pg->d[i][j]->Donor->Position + pg->d[i][j]->offset2)
+			 <=
+			 accSearch - MinDist))
+		{
+		  if (pg->d[i][j]->Strand == cFORWARD)
+			remainder = pg->d[i][j]->Remainder;
+		  else
+			remainder = pg->d[i][j]->Frame;
+		  
+		  if (pg->d[i][j]->GeneScore > pg->Ga[i][remainder]->GeneScore)
+			pg->Ga[i][remainder] = pg->d[i][j];
+		  j++;
+		}
       jUpdate = j;
       
-      /* Second: How many d-exons must we backup? */
+      /* 2. Copy exons needed to recompute maximum distance requirement */
       if (MaxDist == INFI)      
-	jMaxdist = jUpdate;
+		jMaxdist = jUpdate;
       else
-	{
-	  /* Loop back until first exon out of acc-dMax */
-	  j = jUpdate;
-	  while (j>=0 && j < pg->km[i]  && 
-		 ((pg->d[i][j]->Donor->Position 
-		   + pg->d[i][j]->offset2)
-		  >= 
-		  accSearch - MaxDist))
-            j--;
-	  jMaxdist = (j < pg->km[i])? j+1 : j;
-	}
+		{
+		  /* Loop back until reaching first exon out of range: acc-dMax */
+		  j = jUpdate;
+		  while (j>=0 && j < pg->km[i]  && 
+				 ((pg->d[i][j]->Donor->Position 
+				   + pg->d[i][j]->offset2)
+				  >= 
+				  accSearch - MaxDist))
+			j--;
+		  jMaxdist = (j < pg->km[i])? j+1 : j;
+		}
       
-      /* Third: save the current state of d-array */
+      /* 3. To save the set of best genes finished in any selected d-exon */
       for(j = jMaxdist; j < pg->km[i]; j++)
-	{
-	  pg->d[i][j-jMaxdist] =  backupGene(pg->d[i][j], dumpster);
-	  nBackups++;
-	}
+		{
+		  pg->d[i][j-jMaxdist] =  backupGene(pg->d[i][j], dumpster);
+		  nBackups++;
+		}
       pg->km[i] = pg->km[i] - jMaxdist;
       pg->je[i] = jUpdate - jMaxdist;
     }
- 
-  sprintf(mess,"%ld d-genes saved(%ld real exons)\n",
-	  nBackups, dumpster->h->total);
+  
+  sprintf(mess,"%ld d-genes saved(%ld real exons)",
+		  nBackups, dumpster->h->total);
   printMess(mess);
-
 }
 
-/* Clean old backup genes because next sequence begins */
+/* Reset counters and pointers for the next input sequence */
 void cleanGenes(packGenes* pg, int nclass, packDump* dumpster)
 {
-   int aux, aux2;
-
+  int aux, aux2;
+  
   for(aux=0; aux<nclass; aux++)
-  {
-      /* Clean all sort-by-donor array */
+	{
+      /* Reset sort-by-donor functions */
       pg->je[aux] = 0;
       pg->km[aux] = 0;
       
-      /* Reinitialize Ga-exons: every Ga looks at Ghost exon */
+      /* Reset Ga-exons: every Ga looks at Ghost exon */
       for(aux2=0; aux2 < FRAMES; aux2++)
-	pg->Ga[aux][aux2] = pg->Ghost;
-  }
-
-  /* Clean Optimal Gen */
+		pg->Ga[aux][aux2] = pg->Ghost;
+	}
+  
+  /* Reset Optimal Gene */
   pg->GOptim = pg->Ghost;
-
+  
+  /* Reset counters for dumpster arrays if were used before */
   if (MAXBACKUPSITES && MAXBACKUPEXONS)
-  {
-    /* Reset dumpster */
-    dumpster->ndumpExons = 0;
-    dumpster->ndumpSites = 0;
-  }
+	{
+	  dumpster->ndumpExons = 0;
+	  dumpster->ndumpSites = 0;
+	}
 }
 
 
