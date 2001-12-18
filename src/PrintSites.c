3d00caa4ebb3c148ad94bd8da1a9091c3945e6ae
Enrique Blanco <enrique.blanco@crg.eu>
2001-12-18 15:47:46 +0000

    geneid version 1.1. final version:
    includes printing partial signals (filled with "*"'s)

--- src/PrintSites.c
+++ src/PrintSites.c
@@ -2,11 +2,11 @@
 *                                                                        *
 *   Module: PrintSites                                                   *
 *                                                                        *
-*   Print splice sites (ADN sequence, score and position)                *
+*   Print predicted signals (motif, score and position)                  *
 *                                                                        *
-*   This file is part of the geneid Distribution                         *
+*   This file is part of the geneid 1.1 distribution                     *
 *                                                                        *
-*     Copyright (C) 2000 - Enrique BLANCO GARCIA                         *
+*     Copyright (C) 2001 - Enrique BLANCO GARCIA                         *
 *                          Roderic GUIGO SERRA                           * 
 *                                                                        *
 *  This program is free software; you can redistribute it and/or modify  *
@@ -24,14 +24,16 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/
 
-/*  $Id: PrintSites.c,v 1.3 2000-09-01 15:54:45 eblanco Exp $  */
+/*  $Id: PrintSites.c,v 1.4 2001-12-18 15:47:46 eblanco Exp $  */
 
 #include "geneid.h"
 
 extern int GFF;
 
-void PrintSite(site *s, int type, char Name[], int Strand,
-	       char* seq, profile *p)
+/* Print a individual signal according to the selected format */
+void PrintSite(site* s, int type,
+               char Name[], int Strand,
+               char* seq, profile* p)
 {
   int k;
   char Type[MAXTYPE];
@@ -40,10 +42,9 @@ void PrintSite(site *s, int type, char Name[], int Strand,
   char sAux[MAXSTRING];
   int i;
   
-  /* Corrections because of geneid works with coding positions */
-  /* position + k points to the begin of splice signal */
-  /* position + offset points to the end of signal */
-  
+  /* Corrections because of geneid works with coding (useful) positions */
+  /* position + k means "real beginning of the signal" */
+  /* position + offset means "real end of the signal" */
   Type[0] = '\0';
   switch(type)
     {
@@ -65,70 +66,77 @@ void PrintSite(site *s, int type, char Name[], int Strand,
       break;
     }
   
+  /* Depending on the strand: exchange k and offset */
+  /* Incomplete signals are filled in with "*" */
   switch(Strand)
     {
     case FORWARD: strand = '+';
       for(i=0; i < p->dimension; i++)
-	sAux[i] = seq[s->Position - p->offset + i];
+		if (s->Position - p->offset + i >= 0)
+		  sAux[i] = seq[s->Position - p->offset + i];
+		else
+		  sAux[i] = '*';
       sAux[i] = '\0';
       break;
+	  
     case REVERSE: strand = '-';
       k = -k;
-      offset = (-offset);
-
+      offset = -offset;
       ReverseSubSequence(s->Position - (p->dimension - p->offset - 1), 
-			 s->Position + (p->offset),
-			 seq, sAux);
-      
+						 s->Position + (p->offset), seq, sAux);    
       sAux[p->dimension] = '\0'; 
       break;
     }
   
-   /* Formatted output */
-   if (GFF)
-     {
-       /* Print site: gff format */
-       printf("%s\t%s\t%s\t%ld\t%ld\t%5.2f\t%c\t.\t# %s\n",
-	      Name,
-	      SITES,
-	      Type,
-	      (offset>0)? 
-	      s->Position+k+COFFSET : 
-	      s->Position+k+offset+COFFSET,
-	      
-	      (offset>0)? 
-	      s->Position+k+offset+COFFSET : 
-	      s->Position+k+COFFSET,
-	      
-	      s->Score,
-	      strand,
-	      sAux);
-     }
-   else
-     {
-       /* Print site: default format */
-       printf("%8s %8ld %8ld\t%5.2f\t%c\t%s\n",
-	      Type,
-
-	      (offset>0)? 
-	      s->Position+k+COFFSET : 
-	      s->Position+k+offset+COFFSET,
-	      
-	      (offset>0)? 
-	      s->Position+k+offset+COFFSET : 
-	      s->Position+k+COFFSET,
-	      
-	      s->Score,
-	      strand,
-	      sAux);
-     }
+  /* Effective output for this signal */
+  if (GFF)
+	{
+	  /* Print site: gff format */
+	  printf("%s\t%s\t%s\t%ld\t%ld\t%5.2f\t%c\t.\t# %s\n",
+			 Name,
+			 SITES,
+			 Type,
+			 (offset>0)? 
+			 s->Position+k+COFFSET : 
+			 s->Position+k+offset+COFFSET,
+			 
+			 (offset>0)? 
+			 s->Position+k+offset+COFFSET : 
+			 s->Position+k+COFFSET,
+			 
+			 s->Score,
+			 strand,
+			 sAux);
+	}
+  else
+	{
+	  /* Print site: default format */
+	  printf("%8s %8ld %8ld\t%5.2f\t%c\t%s\n",
+			 Type,
+			 
+			 (offset>0)? 
+			 s->Position+k+COFFSET : 
+			 s->Position+k+offset+COFFSET,
+			 
+			 (offset>0)? 
+			 s->Position+k+offset+COFFSET : 
+			 s->Position+k+COFFSET,
+			 
+			 s->Score,
+			 strand,
+			 sAux);
+	}
 }
 
-void PrintSites (site *s, long ns,int type,
-		 char Name[], int Strand,
-		 long l1, long l2,
-		 char* seq,
-		 profile *p)
+/* Print a list of signals according to the selected format */
+void PrintSites (site* s,
+                 long ns,
+                 int type,
+                 char Name[],
+                 int Strand,
+                 long l1, long l2,
+                 char* seq,
+                 profile *p)
 {
   long i;
   char Type[MAXTYPE];
@@ -136,7 +144,8 @@ void PrintSites (site *s, long ns,int type,
   long limit;
   long printPoint;
   
-  /* Avoiding repeated sites because of overlapping splits */
+  /* Skip repeated sites because of overlapping fragments */
+  /* depending on the type of the output signals */
   limit = l1 + OVERLAP;
   i = 0;
   printPoint = ns;
@@ -145,60 +154,62 @@ void PrintSites (site *s, long ns,int type,
     {
     case ACC: strcpy(Type,sACC);
       if ((Strand == REVERSE) && (l1))
-	{
-	  i = ns-1;
-	  while(i>=0 && ((s+i)->Position < limit))
-	    i--;
-	  printPoint = i+1;
-	  i = 0;
-	}
+		{
+		  i = ns-1;
+		  while(i>=0 && ((s+i)->Position < limit))
+			i--;
+		  printPoint = i+1;
+		  i = 0;
+		}
       break;
-    
+	  
     case STA:strcpy(Type,sSTA);
       if ((Strand == REVERSE) && (l1))
-	{
-	  i = ns-1;
-	  while(i>=0 && ((s+i)->Position < limit))
-	    i--;
-	  printPoint = i+1;
-	  i = 0;
-	}
+		{
+		  i = ns-1;
+		  while(i>=0 && ((s+i)->Position < limit))
+			i--;
+		  printPoint = i+1;
+		  i = 0;
+		}
       break;
-
+	  
     case DON:strcpy(Type,sDON);
       if ((Strand == FORWARD) && (l1))
-	{
-	  i = 0;
-	  while(i<ns && ((s+i)->Position < limit))
-	    i++;
-	}
+		{
+		  i = 0;
+		  while(i<ns && ((s+i)->Position < limit))
+			i++;
+		}
       break;
       
     case STO:strcpy(Type,sSTO);
       if ((Strand == FORWARD) && (l1))
-	{
-	  i = 0;
-	  while(i<ns && ((s+i)->Position < limit))
-	    i++;
-	}
+		{
+		  i = 0;
+		  while(i<ns && ((s+i)->Position < limit))
+			i++;
+		}
       if ((Strand == REVERSE) && (l1))
-	{
-	  i = ns-1;
-	  while(i>=0 && ((s+i)->Position < limit))
-	    i--;
-	  printPoint = i+1;
-	  i = 0;
-	}
+		{
+		  i = ns-1;
+		  while(i>=0 && ((s+i)->Position < limit))
+			i--;
+		  printPoint = i+1;
+		  i = 0;
+		}
       break;
     }
-
+  
   strand = (Strand == FORWARD)? '+' : '-';
   
-  printf("# %ss(%c) predicted in sequence %s: [%ld,%ld]\n", 
-	 Type,
-	 strand,
-	 Name, l1, l2);
+  /* Output header for the input (list, fragment) */
+  printf("# %ss(%c) predicted in sequence %s: [%ld,%ld]\n",
+		 Type,
+		 strand,
+		 Name, l1, l2);
   
+  /* Print part of the list by computing the values i and printPoint */
   for (; i < printPoint; i++)
     PrintSite((s+i),type,Name,Strand,seq,p);
 }
