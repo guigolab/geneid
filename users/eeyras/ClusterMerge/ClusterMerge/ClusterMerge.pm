672660bbcad674ca17dd194681db1272a04ee396
Eduardo Eyras <eduardo.eyras@upf.edu>
2004-06-03 13:32:38 +0000

    added license and publication

--- users/eeyras/ClusterMerge/ClusterMerge/ClusterMerge.pm
+++ users/eeyras/ClusterMerge/ClusterMerge/ClusterMerge.pm
@@ -1,11 +1,31 @@
+##########################################################################
+#                                                                        #    
+#  Written by Eduardo Eyras                                              #
+#                                                                        #
+#  This program is free software; you can redistribute it and/or modify  #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation; either version 2 of the License, or     #
+#  (at your option) any later version.                                   #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with this program; if not, write to the Free Software           #
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             #
+##########################################################################
+
+
+######################################################################
 #
-# Written by Eduardo Eyras
+#  If you use this program in your analyses, please cite:
 #
-# Copyright GRL & EBI
-#
-# You may distribute this module under the same terms as perl itself
-#
-# POD documentation - main docs before the code
+#  Eyras E, Caccamo M, Curwen V, Clamp M.
+#  ESTGenes: alternative splicing from ESTs in Ensembl.
+#  Genome Res. 2004 May;14(5):976-87. 
+######################################################################
 
 =pod 
 
