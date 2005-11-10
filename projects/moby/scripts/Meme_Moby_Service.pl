cdde1c6e7c7a2a4c767c8c5ea5c00d06af6971ff
Arnau Bria <arnau.bria@crg.eu>
2005-11-10 14:30:09 +0000

    testing perl client forrunMemeText and runMemeHTML

---
+++ projects/moby/scripts/Meme_Moby_Service.pl
@@ -0,0 +1,301 @@
+#!/usr/local/bin/perl -w
+
+##################################################################
+#
+# MEME Moby Service Client
+#
+##################################################################
+
+use strict;
+use Data::Dumper;
+
+# be prepare for command-line options/arguments
+use Getopt::Std;
+
+# BioMoby and SOAP libraries
+
+use MOBY::Client::Central;
+use MOBY::Client::Service;
+use SOAP::Lite;
+# use SOAP::Lite + 'trace';
+
+# Bioperl Libraries
+
+use Bio::SeqIO;
+
+# Benchmark Module
+use Benchmark;
+##################################################################
+
+sub help {
+return <<"END_HELP";
+Description: Execute Meme Moby services available from genome.imim.es
+Usage:
+
+    MatScan_Moby_Service.pl [-h] -x {Moby Central} -s {Service Name} -f {sequence FASTA file}
+	-h help
+	-x MOBY Central: Chirimoyo, Xistral, Inab or BioMoby
+		<1> or Chirimoyo
+		<2> or Xistral
+		<3> or Inab
+		<4> or BioMoby
+	-s Service Name (runMemeText or runMemeHTML)
+	-i Sequence(s) input file, in FASTA format - optional
+	
+Examples using some combinations:
+	perl Meme_Moby_Service.pl -x 1 -s runMemeText -f /home/ug/arnau/data/promoterExtraction/Homo_sapiens.1000.intergenic.fa
+
+END_HELP
+
+}
+
+BEGIN {
+	
+	# Determines the options with values from program
+	use vars qw/$opt_h $opt_x $opt_s $opt_f/;
+	   
+	# these are switches taking an argument (a value)
+	my $switches = 'hxsf';
+	   
+	# Get the switches
+	getopt($switches);
+	
+	# If the user does not write nothing, skip to help
+	if (defined($opt_h) || !defined($opt_x) || !defined($opt_s)){
+		print help;
+		exit 0;
+	}
+	
+}
+
+my $t1 = Benchmark->new ();
+
+my $_debug = 0;
+
+##################################################################
+#
+# Setup sequence input data and moby parameters
+#
+##################################################################
+
+my $serviceName = $opt_s;
+my $articleName = "sequences";
+$::authURI = 'genome.imim.es';
+
+my $serviceType = "Collection";
+
+my $in_file_1    = $opt_f || "/home/ug/arnau/data/promoterExtraction/mut1_downreg.1000.intergenic.fa";
+$in_file_1    = "/home/ug/arnau/data/promoterExtraction/Homo_sapiens.1000.intergenic.fa";
+my $datasource = "EMBL";
+
+# Parameters
+
+my $evalue_cutoff = 1000;
+
+##################################################################
+#
+# Setup Moby configuration parameters
+#
+##################################################################
+
+##############################################
+#
+# ASSIGN THE MOBY URI AND MOBY SERVER
+#
+##############################################
+
+my $URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://chirimoyo.ac.uma.es/cgi-bin/MOBY-Central.pl';
+my $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
+my $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+if (defined($opt_x)) {
+
+	# Delete spaces
+	$opt_x =~ s/\s//g;
+
+	# Assign the MOBY Server and MOBY URI
+	if (($opt_x == 1) || ($opt_x eq 'Chirimoyo')) {
+	    
+	    # export MOBY_URI
+	    # export MOBY_SERVER
+	    $URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://chirimoyo.ac.uma.es/cgi-bin/MOBY-Central.pl';
+	    $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
+	    $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+	
+	}elsif (($opt_x == 2) || ($opt_x eq 'Xistral')) {
+	
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://xistral/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://xistral/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+	}elsif (($opt_x == 3) || ($opt_x eq 'Inab')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://www.inab.org/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://www.inab.org/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+	
+	}elsif (($opt_x == 4) || ($opt_x eq 'BioMoby')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://mobycentral.icapture.ubc.ca/cgi-bin/MOBY05/mobycentral.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://mobycentral.icapture.ubc.ca/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+	}else {
+		print help;
+		exit 0;
+	}
+
+}else {
+	print help;
+	exit 0;
+}
+
+##################################################################
+#
+# Moby Server Instanciation
+#
+##################################################################
+
+if ($_debug) {
+    print STDERR "Instanciating Central object...\n";
+}
+
+my $C = MOBY::Client::Central->new(
+				   Registries => {mobycentral => {URL => $URL,URI => $URI}}
+				   );
+
+if (defined $C) {
+    if ($_debug) {
+	print STDERR "Instanciation done.\n";
+	print STDERR "TESTING MOBY CLIENT with\n\tURL: $URL\n\tURI: $URI\n\tProxy: $PROXY\n\n";
+    }
+}
+else {
+    print STDERR "Error, Central could not be instanciated!\n";
+    exit 0;
+}
+
+##################################################################
+#
+# Moby Service instantiation
+#
+##################################################################
+
+if ($_debug) {
+    print STDERR "finding service, $serviceName...\n";
+}
+
+my ($service_instances, $reg) = $C->findService (
+						 serviceName  => "$serviceName",
+						 authURI      => $::authURI
+						 );
+
+if ($_debug) {
+    print STDERR "Service instances references: " . @$service_instances . "\n";
+}
+
+if (@$service_instances == 0) {
+    print STDERR "Error, can't find any service called $serviceName!\n";
+    exit 0;
+}
+
+my $service_instance = $service_instances->[0];
+
+my $wsdl = $C->retrieveService($service_instance);
+
+if ($_debug) {
+    print STDERR "wsdl: $wsdl\n";
+}
+
+if (!$wsdl || ($wsdl !~ /\<definitions/)){
+    print "test \t\t[FAIL]\tWSDL was not retrieved\n\n";
+}
+
+my $Service = MOBY::Client::Service->new(service => $wsdl);
+
+##################################################################
+#
+# Instanciate a Bioperl Sequence Factory to parse the sequence entries from the FASTA file
+#
+##################################################################
+
+my $inputs = [];
+my $files = [$in_file_1];
+my $i = 0;
+while ($i < @$files) {
+    my $seqin = Bio::SeqIO->new (
+				 -file   => $files->[$i],
+				 -format => 'fasta',
+				 );
+
+    while (my $seqobj = $seqin->next_seq) {
+	my $nucleotides  = $seqobj->seq;
+	my $seq_id       = $seqobj->display_id;
+	my $lnucleotides = length($nucleotides);
+	
+	##################################################################
+	#
+	# Set up the service input and the secondary articles in XML format 
+	#
+	##################################################################
+	
+	#
+	# Sequence Input
+	#
+	
+	my $input = <<PRT;
+<DNASequence namespace="$datasource" id="$seq_id">
+<Integer namespace="" id="" articleName="Length">$lnucleotides</Integer>
+<String namespace="" id=""  articleName="SequenceString">$nucleotides</String>
+</DNASequence>
+PRT
+	    
+	    push (@$inputs, $input);
+    }
+
+    $i++;
+}
+
+#
+# Parameters (secondary articles)
+#
+
+my $evalue_cutoff_xml = <<PRT;
+<Value>$evalue_cutoff</Value>
+PRT
+
+# ...
+
+##################################################################
+#
+# Service execution
+#
+##################################################################
+
+my $result;
+
+if ($serviceType eq "Collection") {
+
+    $result = $Service->execute(XMLinputlist => [
+						 ["$articleName", $inputs, 'motif E-value cutoff', $evalue_cutoff_xml]
+						 ]);
+}
+
+##################################################################
+#
+# Result processing
+#
+##################################################################
+
+print STDERR "result\n";
+print $result;
+print STDERR "\n";
+
+my $t2 = Benchmark->new ();
+print STDERR "\nTotal : ", timestr (timediff ($t2, $t1)), "\n";
