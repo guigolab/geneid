a4f67cf0998870d9271572d48fe1902a46848f9e
Arnau Bria <arnau.bria@crg.eu>
2005-11-28 13:14:37 +0000

    added a new service

--- projects/moby/scripts/testServices/testServices.pl
+++ projects/moby/scripts/testServices/testServices.pl
@@ -81,10 +81,11 @@ my $gostat_regulated_xml_file = "mut1_downreg.fbgn.xml";
 my $gostat_allArray_xml_file  = "allArray.fbgn.xml";
 my $ENSG00000197785_upstream_sequence_xml_file = "ENSG00000197785.xml";
 my $ENSG00000197785_matscan_xml_file           = "ENSG00000197785.runMatScanGFF.xml";
+my $ENSG00000197785_fasta_xml_file             = "ENSG00000197785.fa.xml";
 
 # Check that the files exist !!!
 
-if ((not -f "$input_data_dir/$nucleotide_sequence_xml_file") || (not -f "$input_data_dir/$tblastx_output_xml_file") || (not -f "$input_data_dir/$geneIds_lst_xml_file") || (not -f "$input_data_dir/$GeneIDGFF_xml_file") || (not -f "$input_data_dir/$gostat_regulated_xml_file") || (not -f "$input_data_dir/$gostat_allArray_xml_file") || (not -f "$input_data_dir/$ENSG00000197785_upstream_sequence_xml_file") || (not -f "$input_data_dir/$ENSG00000197785_matscan_xml_file")) {
+if ((not -f "$input_data_dir/$nucleotide_sequence_xml_file") || (not -f "$input_data_dir/$tblastx_output_xml_file") || (not -f "$input_data_dir/$geneIds_lst_xml_file") || (not -f "$input_data_dir/$GeneIDGFF_xml_file") || (not -f "$input_data_dir/$gostat_regulated_xml_file") || (not -f "$input_data_dir/$gostat_allArray_xml_file") || (not -f "$input_data_dir/$ENSG00000197785_upstream_sequence_xml_file") || (not -f "$input_data_dir/$ENSG00000197785_matscan_xml_file") || (not -f "$input_data_dir/$ENSG00000197785_fasta_xml_file")) {
     print STDERR "Error, can't find one of the input files in directory, $input_data_dir!\n";
     exit 0;
 }
@@ -97,19 +98,21 @@ my $gostat_regulated_xml = qx/cat $input_data_dir\/$gostat_regulated_xml_file/;
 my $gostat_allArray_xml  = qx/cat $input_data_dir\/$gostat_allArray_xml_file/;
 my $ENSG00000197785_upstream_sequence_xml = qx/cat $input_data_dir\/$ENSG00000197785_upstream_sequence_xml_file/;
 my $ENSG00000197785_matscan_xml           = qx/cat $input_data_dir\/$ENSG00000197785_matscan_xml_file/;
+my $ENSG00000197785_fasta_xml             = qx/cat $input_data_dir\/$ENSG00000197785_fasta_xml_file/;
 
 my $runGeneID_control_file                  = "Hsap_BTK.msk.runGeneID.control";
 my $runGeneIDGFF_control_file               = "Hsap_BTK.msk.runGeneIDGFF.control";
 my $runSGP2GFF_control_file                 = "Hsap_BTK.msk.runSGP2GFF.control";
 my $translateGeneIDGFFPredictions_control_file = "Hsap_BTK.msk.GeneIDGFF.translateGeneIDGFFPredictions.control";
 my $getUpstreamSeqfromEnsembl_control_file  = "geneIds.lst.getUpstreamSeqfromEnsembl.control";
-my $runGOstat_control_file = "mut1_downreg.fbgn.runGOstat.control";
+my $runGOstat_control_file                  = "mut1_downreg.fbgn.runGOstat.control";
 my $fromGenericSequencetoFASTA_control_file = "Hsap_BTK.msk.fromGenericSequencetoFASTA.control";
 my $fromGenericSequenceCollectiontoFASTA_control_file = "Hsap_BTK.msk.fromGenericSequenceCollectiontoFASTA.control";
 my $runMatScanGFF_control_file              = "ENSG00000197785.runMatScanGFF.control";
 my $runMatScanGFFCollection_control_file    = "ENSG00000197785.runMatScanGFFCollection.control";
 my $runMetaAlignment_control_file           = "ENSG00000197785.runMetaAlignment.control";
 my $runMetaAlignmentGFF_control_file        = "ENSG00000197785.runMetaAlignmentGFF.control";
+my $fromFASTAtoDNASequenceCollection_control_file     = "ENSG00000197785.fromFASTAtoDNASequenceCollection.control";
 
 ##################################################################
 #
@@ -546,6 +549,41 @@ if (defined $service) {
 	unlink $results_file;
     }
     
+}
+
+# Execute fromFASTAtoDNASequenceCollection Web service
+
+print STDERR "\ntesting fromFASTAtoDNASequenceCollection...\n\n";
+
+$service = MobyServiceInstantiation ($C, "fromFASTAtoDNASequenceCollection", $AUTH);
+if (defined $service) {
+    my $result = $service->execute(
+				   XMLinputlist => [
+						    ['sequences', $ENSG00000197785_fasta_xml]
+						    ]
+				   );
+    
+    my ($results_fh, $results_file) = tempfile ("/tmp/MOBY_RESULTS.XXXXX", UNLINK => 0);
+    
+    print $results_fh "$result\n";
+    
+    my @diff_results = qx/diff $control_data_dir\/$fromFASTAtoDNASequenceCollection_control_file $results_file/;    
+    
+    if ((@diff_results > 0) && (! ($diff_results[1] =~ /date/))) {
+	print STDERR "fromFASTAtoDNASequenceCollection service failed!\n";
+	print STDERR "diff_results: @diff_results\n";
+	
+	close $results_fh;
+	unlink $results_file;
+	
+    }
+    else {
+	
+	print STDERR "fromFASTAtoDNASequenceCollection okay...\n";
+	
+	close $results_fh;
+	unlink $results_file;
+    }
 }
 
 # Execute runSGP2GFF Web service
