170a4dd9b76f6b458f99aecccbcb898d823d9926
Arnau Bria <arnau.bria@crg.eu>
2005-12-01 14:34:21 +0000

    promoter_extraction.pl

--- users/arnau/projects/promoter_extraction/config.pl
+++ users/arnau/projects/promoter_extraction/config.pl
@@ -1,5 +1,23 @@
+##################################################################################
+#
+# Configuration file for promoter sequence extraction from Ensembl Database
+#
+##################################################################################
+
+# bioperl libraries - at least bioperl v1.2 is required
+use lib "/usr/local/Install/perl-5.8.6/lib/site_perl/5.8.6/";
+
 # Data::UUID
+# required for Ensembl compara
 use lib "/home/ug/gmaster/lib/site_perl/5.8.5/i686-linux-thread-multi";
+
+#########################################
+#
+# Libraries Path - Modify to fit your system !
+#
+#########################################
+
+$_libraries_path = "/home/ug/gmaster/projects/promoter_extraction/lib";
 
 # Ensembl database parameters
 
