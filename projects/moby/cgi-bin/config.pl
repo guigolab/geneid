6033a96e06bdfc8894c30ee6a35228947b9e1d25
Gmaster <gmaster@crg.eu>
2006-01-30 15:24:49 +0000

    added in the path SOAP 0.67 libraries, but commented as it is not stable yet

--- projects/moby/cgi-bin/config.pl
+++ projects/moby/cgi-bin/config.pl
@@ -5,7 +5,11 @@ use lib "/home/ug/gmaster/projects/moby/prod/lib";
 use lib "/home/ug/gmaster/projects/moby/biomoby.0.8.2a/Perl";
 
 # Also need SOAP and Bioperl libraries that are already installed in the default perl library path
-# /usr/local/Install/perl-5.8.5/lib/site_perl/5.8.5/Bio/
+# /usr/local/Install/perl-5.8.5/lib/site_perl/5.8.5/Bio
+
+# SOAP v0.67
+# use lib "/home/ug/gmaster/projects/lib/5.8.5/i686-linux-thread-multi";
+# use lib "/home/ug/gmaster/projects/lib/site_perl/5.8.5";
 
 # GOstat setup
 
