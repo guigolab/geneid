3da63559475bfd9b0bdc021e6a8415c12397c15d
Arnau Bria <arnau.bria@crg.eu>
2006-02-22 17:32:38 +0000

    Added error reporting

--- projects/moby/scripts/workflows_implementations/GenesClustering_FASTA.pl
+++ projects/moby/scripts/workflows_implementations/GenesClustering_FASTA.pl
@@ -7,6 +7,7 @@
 ##################################################################
 
 use strict;
+use Data::Dumper;
 
 # be prepare for command-line options/arguments
 use Getopt::Std;
@@ -25,6 +26,10 @@ use MIME::Base64;
 
 # Benchmark Module
 use Benchmark;
+
+# HTML encoding/decoding module
+use HTML::Entities;
+
 ##################################################################
 
 sub help {
@@ -77,7 +82,7 @@ my $_debug = 0;
 
 ##################################################################
 #
-# Setup sequence input data and moby parameters
+# Setup sequences input data and moby parameters
 #
 ##################################################################
 
@@ -102,13 +107,16 @@ else {
   print STDERR "don't know about the value for method parameter, $method!";
   exit 1;
 }
+
+my $method_xml = "<Value>$method</Value>";
+
 my $threshold = $opt_t || 0.85;
 my $database  = $opt_d || "transfac";
 
-# parameter file
+my $threshold_xml   = "<Value>$threshold</Value>";
+my $matrix_xml      = "<Value>$database</Value>";
 
-# generate a hash of hash
-# $parameters{$serviceName} = $serviceName_parameters  
+# parameters configuration file parsing
 
 my $serviceName = undef;
 
@@ -135,10 +143,6 @@ if (defined $output_dir) {
 #
 ##################################################################
 
-# Get input information from config file
-# works this way ????
-
-# my $input_parameter = $parameters{input};
 my $namespace = $parameters{input}->{namespace};
 if ($_debug) {
   print STDERR "namespace, $namespace\n";
@@ -236,7 +240,7 @@ if ($input_type eq "FASTA") {
     
     $input_xml = <<PRT;
 <FASTA namespace="$namespace" id="">
-<String articleName="Content">
+<String articleName="content">
 <![CDATA[
 $input
 ]]>
@@ -253,7 +257,7 @@ else {
     
     $input_xml = <<PRT;
 <text-formatted namespace="$namespace" id="">
-<String articleName="Content">
+<String articleName="content">
 <![CDATA[
 $input
 ]]>
@@ -317,31 +321,38 @@ my $Service = getService ($C, $serviceName, $authURI);
 #
 ##################################################################
 
-my $result;
+my $moby_response;
 if ($input_type eq "FASTA") {
-    $result = $Service->execute (XMLinputlist => [
+    $moby_response = $Service->execute (XMLinputlist => [
 						  ["$articleName", $input_xml],
 						 ]);
 }
 else {
-    $result = $Service->execute (XMLinputlist => [
+    $moby_response = $Service->execute (XMLinputlist => [
 						  ["$articleName", $input_xml, "species", $species_xml, "upstream_length", $upstream_length_xml, "downstream_length", $downstream_length_xml, "intergenic_only", $intergenic_only_xml]
 						 ]);
 }
 
 ##################################################################
 #
-# Result processing
+# Moby_Response processing
 #
 ##################################################################
 
 if ($_debug) {
-	print STDERR "result\n";
-	print STDERR $result;
+	print STDERR "$serviceName results\n";
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
-$input_xml = parseResults ($result, "DNASequence");
+if (hasFailed ($moby_response)) {
+    print STDERR "service, $serviceName, has failed!\n";
+    my $moby_error_message = getExceptionMessage ($moby_response);
+    print STDERR "reason is the following,\n$moby_error_message\n";
+    exit 1;
+}
+
+$input_xml = parseResults ($moby_response, "DNASequence");
 
 if ($_debug) {
 	print STDERR "input xml for next service:\n";
@@ -357,29 +368,29 @@ print STDERR "Executing MatScan...\n";
 $serviceName        = "runMatScanGFFCollection";
 $authURI            = $parameters{$serviceName}->{authURI}     || die "no URI for $serviceName\n";
 $articleName        = $parameters{$serviceName}->{articleName} || die "article name for $serviceName\n";
-# Set it up from the command-line now
-# my $threshold       = $parameters{$serviceName}->{threshold}   || die "no threshold for $articleName\n";
-# my $database        = $parameters{$serviceName}->{database}    || die "no matrix database for $articleName\n";
-my $matrix_mode     = $parameters{$serviceName}->{matrix_mode} || die "no matrix mode for $articleName\n";
-my $threshold_xml   = "<Value>$threshold</Value>";
-my $matrix_xml      = "<Value>$database</Value>";
-my $matrix_mode_xml = "<Value>$matrix_mode</Value>";
 
 $Service = getService ($C, $serviceName, $authURI);
 
-$result = $Service->execute (XMLinputlist => [
-					      ["$articleName", $input_xml, "threshold", $threshold_xml, "matrix", $matrix_xml, "matrix mode", $matrix_mode_xml]
+$moby_response = $Service->execute (XMLinputlist => [
+					      ["$articleName", $input_xml, "threshold", $threshold_xml, "matrix", $matrix_xml]
 					     ]);
 
 if ($_debug) {
-	print STDERR "result\n";
-	print STDERR $result;
+	print STDERR "$serviceName results\n";
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
-$input_xml = parseResults ($result, "GFF");
+if (hasFailed ($moby_response)) {
+    print STDERR "service, $serviceName, has failed!\n";
+    my $moby_error_message = getExceptionMessage ($moby_response);
+    print STDERR "reason is the following,\n$moby_error_message\n";
+    exit 1;
+}
+
+$input_xml = parseResults ($moby_response, "GFF");
 if (defined $output_dir) {
-  saveResults ($result, "GFF", "MatScan", $output_dir);
+  saveResults ($moby_response, "GFF", "MatScan", $output_dir);
 }
 
 if ($_debug) {
@@ -401,19 +412,26 @@ $articleName = $parameters{$serviceName}->{articleName} || die "article name for
 
 $Service = getService ($C, $serviceName, $authURI);
 
-$result = $Service->execute (XMLinputlist => [
+$moby_response = $Service->execute (XMLinputlist => [
 					      ["$articleName", $input_xml]
 					     ]);
 
 if ($_debug) {
 	print STDERR "$serviceName result\n";
-	print STDERR $result;
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
-$input_xml = parseResults ($result, "text-formatted");
+if (hasFailed ($moby_response)) {
+    print STDERR "service, $serviceName, has failed!\n";
+    my $moby_error_message = getExceptionMessage ($moby_response);
+    print STDERR "reason is the following,\n$moby_error_message\n";
+    exit 1;
+}
+
+$input_xml = parseResults ($moby_response, "text-formatted");
 if (defined $output_dir) {
-  saveResults ($result, "text-formatted", "Meta", $output_dir);
+  saveResults ($moby_response, "text-formatted", "Meta", $output_dir);
 }
 
 if ($_debug) {
@@ -434,19 +452,26 @@ $articleName = $parameters{$serviceName}->{articleName} || die "article name for
 
 $Service = getService ($C, $serviceName, $authURI);
 
-$result = $Service->execute (XMLinputlist => [
+$moby_response = $Service->execute (XMLinputlist => [
 					      ["$articleName", $input_xml]
 					     ]);
 
 if ($_debug) {
-	print STDERR "$serviceName result\n";
-	print STDERR $result;
+	print STDERR "$serviceName results\n";
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
-my $input_xml_aref = parseResults ($result, "MicroArrayData_Text");
+if (hasFailed ($moby_response)) {
+    print STDERR "service, $serviceName, has failed!\n";
+    my $moby_error_message = getExceptionMessage ($moby_response);
+    print STDERR "reason is the following,\n$moby_error_message\n";
+    exit 1;
+}
+
+my $input_xml_aref = parseResults ($moby_response, "MicroArrayData_Text");
 if (defined $output_dir) {
-  saveResults ($result, "MicroArrayData_Text", "score_matrix", $output_dir);
+  saveResults ($moby_response, "MicroArrayData_Text", "score_matrix", $output_dir);
 }
 
 if ($_debug) {
@@ -492,25 +517,31 @@ if ($_debug) {
 $serviceName   = "inbHierarchicalCluster";
 $authURI       = $parameters{$serviceName}->{authURI}     || die "no URI for $serviceName\n";
 $articleName   = $parameters{$serviceName}->{articleName} || "";
-# set it up from the command-line now
-# my $method     = $parameters{$serviceName}->{method}      || die "no method for $articleName\n";
-my $method_xml = "<Value>$method</Value>";
 
 $Service = getService ($C, $serviceName, $authURI);
 
-$result = $Service->execute (XMLinputlist => [
+$moby_response = $Service->execute (XMLinputlist => [
 					      ["$articleName", "$input_xml\n", "method", $method_xml]
 					     ]);
 
 if ($_debug) {
-	print STDERR "\n$serviceName result\n";
-	print STDERR $result;
+	print STDERR "\n$serviceName results\n";
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
-$input_xml_aref = parseResults ($result, "Clustering");
+# Check if we got results
+my $newick_trees = parseTextContent ($moby_response, "Newick_Text");
+my $newick_tree  = $newick_trees->[0];
+
+if ($newick_tree eq "") {
+    print STDERR "Hierachical clustering failed!\n";
+    exit 1;
+}
+
+$input_xml_aref = parseResults ($moby_response, "Clustering");
 if (defined $output_dir) {
-  saveResults ($result, "Newick_Text", "newick", $output_dir);
+  saveResults ($moby_response, "Newick_Text", "newick", $output_dir);
 }
 
 if ($_debug) {
@@ -534,20 +565,20 @@ $articleName   = $parameters{$serviceName}->{articleName} || "";
 
 $Service = getService ($C, $serviceName, $authURI);
 
-$result = $Service->execute (XMLinputlist => [
+$moby_response = $Service->execute (XMLinputlist => [
 					      ["$articleName", $input_xml]
 					     ]);
 
 if ($_debug) {
-	print STDERR "$serviceName result\n";
-	print STDERR $result;
+	print STDERR "$serviceName results\n";
+	print STDERR $moby_response;
 	print STDERR "\n";
 }
 
 if (defined $output_dir) {
-  saveResults ($result, "b64_Encoded_PNG", "clustering_tree", $output_dir);
+  saveResults ($moby_response, "b64_Encoded_PNG", "clustering_tree", $output_dir);
 }
-my $picture_b64_aref = parseTextContent ($result, "b64_Encoded_PNG");
+my $picture_b64_aref = parseTextContent ($moby_response, "b64_Encoded_PNG");
 my $picture_b64 = $picture_b64_aref->[0];
 
 # Convert in to a picture and store into a file
@@ -674,19 +705,21 @@ sub parseResults {
     
     if ($size == 0) {
 	$elements = $XML->getElementsByTagName( "object_type" );
+	$size = $elements->size();
 	if ($size == 0) {
 	    print STDERR "Error, can't parse the moby output from the moby XML...\n";
 	}
     }
     
     my $i = 0;
-    while (my $element = $elements->get_node($i)) {
+    while ($i < $size) {
+	my $element   = $elements->get_node ($i);
 	my $input_xml = $element->toString();
 	push (@$inputs_xml, $input_xml);
 	
 	$i++;
     }
-
+    
     return $inputs_xml;
 }
 
@@ -702,16 +735,18 @@ sub parseTextContent {
     
     if ($size == 0) {
       $elements = $XML->getElementsByTagName ("$object_type");
+      $size = $elements->size();
       if ($size == 0) {
         print STDERR "Error, can't parse the moby output from the moby XML...\n";
       }
     }
-    
+
     my $i = 0;
-    while (my $element = $elements->get_node ($i)) {
-      my $input_text = $XML->textContent();
-      push (@$inputs_text, $input_text);
-      $i++;
+    while ($i < $size) {
+	my $element    = $elements->get_node ($i);
+	my $input_text = $element->textContent();
+	push (@$inputs_text, $input_text);
+	$i++;
     }
 
     return $inputs_text;
@@ -769,7 +804,7 @@ sub saveResults {
     $i++;
   }
 
-  my $suffix = getSuffix ($object_type);
+  my $suffix = getFileNameSuffix ($object_type);
 
   # Save on the local disk the data
   my @ids = keys (%inputs_text);
@@ -782,6 +817,12 @@ sub saveResults {
     }
     
     my $data = $inputs_text{$id};
+    # clean data
+    while ($data =~ s/^\n//) {
+    }
+    while (chomp $data) { # just chomp...
+    }
+    
     open FILE, ">$output_dir/$filename" or die "can' open output file, $output_dir/$filename";
     print FILE "$data";
     close FILE;
@@ -797,6 +838,12 @@ sub saveResults {
     foreach my $id (@ids) {
       my $filename = $id . "." . $softwareName . "." . $suffix;
       my $data = $inputs_text{$id};
+      # clean data
+      while ($data =~ s/^\n//) {
+      }
+      while (chomp $data) { # just chomp...
+      }
+
       open FILE, ">$output_dir/$subdir/$filename" or die "can' open output file, $output_dir/$subdir/$filename";
       print FILE "$data";
       close FILE;
@@ -804,7 +851,10 @@ sub saveResults {
   }
 }
 
-sub getSuffix {
+# Set up a file name suffix giving the input object type
+# e.g. if the object is GFF, the file suffix will be '.gff'
+
+sub getFileNameSuffix {
   my ($object_type) = @_;
   my $suffix = "txt";
   
@@ -822,3 +872,49 @@ sub getSuffix {
   
   return $suffix;
 }
+
+
+# parse the moby exception status
+sub hasFailed {
+    my ($moby_response) = @_;
+    
+    # print STDERR "XML Dumping, before, $moby_response\n";
+
+    # convert 
+    # "&lt;Notes&gt;Service execution succeeded&lt;/Notes&gt;"
+    # into
+    # "<Notes>Service execution succeeded</Notes>"
+    
+    $moby_response = HTML::Entities::decode( $moby_response );
+    
+    if (($moby_response =~ /service execution succeeded/i) || ($moby_response =~ /exceptionCode>700/)) {
+	# succeedded
+	return 0;
+    }
+    else {
+	# failed
+	return 1;
+    }
+}
+
+sub getExceptionMessage {
+    my ($moby_response) = @_;
+    
+    # convert 
+    # "&lt;Notes&gt;Service execution succeeded&lt;/Notes&gt;"
+    # into
+    # "<Notes>Service execution succeeded</Notes>"
+    
+    $moby_response = HTML::Entities::decode( $moby_response );
+    my $exceptionMessages = parseTextContent ($moby_response, "exceptionMessage");
+    
+    my $exceptionMessage = $exceptionMessages->[0];
+    # Clean the message
+    while ($exceptionMessage =~ s/^\n//) {
+    }
+    while (chomp $exceptionMessage) { # just chomp... 
+    }
+    
+    return $exceptionMessage;
+}
+
