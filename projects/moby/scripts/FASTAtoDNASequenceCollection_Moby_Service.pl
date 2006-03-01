1497315962dcdefa47ded29b191b4c40ab4df5cf
Arnau Bria <arnau.bria@crg.eu>
2006-03-01 11:55:43 +0000

    Added Mobydev registry

--- projects/moby/scripts/FASTAtoDNASequenceCollection_Moby_Service.pl
+++ projects/moby/scripts/FASTAtoDNASequenceCollection_Moby_Service.pl
@@ -30,9 +30,9 @@ Usage:
 
     GenericSequencetoFASTA_Moby_Service.pl [-h] -x {Moby Central} -s {Service Name} -f {sequence FASTA file}
 	-h help
-	-x MOBY Central: Chirimoyo, Xistral, Inab or BioMoby
+	-x MOBY Central: Chirimoyo, Mobydev, Inab or BioMoby
 		<1> or Chirimoyo
-		<2> or Xistral
+		<2> or Mobydev
 		<3> or Inab
 		<4> or BioMoby
 	-s Service Name - not required right now as only one!
@@ -116,12 +116,12 @@ if (defined($opt_x)) {
 	    $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
 	    $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
 	
-	}elsif (($opt_x == 2) || ($opt_x eq 'Xistral')) {
+	}elsif (($opt_x == 2) || ($opt_x eq 'Mobydev')) {
 	
 		# export MOBY_URI
 		# export MOBY_SERVER
-		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://xistral/cgi-bin/MOBY-Central.pl';
-		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://xistral/MOBY/Central';
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://moby-dev.inab.org/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://moby-dev.inab.org/MOBY/Central';
 		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
 
 	}elsif (($opt_x == 3) || ($opt_x eq 'Inab')) {
@@ -215,12 +215,15 @@ my $Service = MOBY::Client::Service->new(service => $wsdl);
 
 my $fasta_sequences = qx/cat $in_file/;
 
+# <String namespace="" id="" articleName="content">
+# </String>
+
 my $fasta_xml = <<PRT;
-<FASTA namespace="$datasource" id="id">
+<FASTA_NA namespace="$datasource" id="id">
 <![CDATA[
 $fasta_sequences
 ]]>
-</FASTA>
+</FASTA_NA>
 PRT
 
 if ($_debug) {
--- projects/moby/scripts/GenericSequencetoFASTA_Moby_Service.pl
+++ projects/moby/scripts/GenericSequencetoFASTA_Moby_Service.pl
@@ -33,9 +33,9 @@ Usage:
 
     GenericSequencetoFASTA_Moby_Service.pl [-h] -x {Moby Central} -s {Service Name} -f {sequence FASTA file} -t {sequence format type}
 	-h help
-	-x MOBY Central: Chirimoyo, Xistral, Inab or BioMoby
+	-x MOBY Central: Chirimoyo, Mobydev, Inab or BioMoby
 		<1> or Chirimoyo
-		<2> or Xistral
+		<2> or Mobydev
 		<3> or Inab
 		<4> or BioMoby
 	-s Service Name
@@ -122,12 +122,12 @@ if (defined($opt_x)) {
 	    $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
 	    $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
 	
-	}elsif (($opt_x == 2) || ($opt_x eq 'Xistral')) {
+	}elsif (($opt_x == 2) || ($opt_x eq 'Mobydev')) {
 	
 		# export MOBY_URI
 		# export MOBY_SERVER
-		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://xistral/cgi-bin/MOBY-Central.pl';
-		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://xistral/MOBY/Central';
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://moby-dev.inab.org/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://moby-dev.inab.org/MOBY/Central';
 		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
 
 	}elsif (($opt_x == 3) || ($opt_x eq 'Inab')) {
