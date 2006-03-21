771dc20137d63b18dbf60f04f434d7c5526319c9
Gmaster <gmaster@crg.eu>
2006-03-21 21:19:27 +0000

    moved here parseMemeMotifs service

--- projects/moby/lib/INB/GRIB/Services/ParsingServices.pm
+++ projects/moby/lib/INB/GRIB/Services/ParsingServices.pm
@@ -1,4 +1,4 @@
-# $Id: ParsingServices.pm,v 1.1 2006-03-21 17:51:11 gmaster Exp $
+# $Id: ParsingServices.pm,v 1.2 2006-03-21 21:19:27 gmaster Exp $
 #
 # This file is an instance of a template written 
 # by Roman Roset, INB (Instituto Nacional de Bioinformatica), Spain.
@@ -110,6 +110,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 # 
 our @EXPORT = qw(
   &fromMetaAlignmentstoScoreMatrix
+  &parseMotifMatricesFromMEME
 );
 
 our $VERSION = '1.0';
@@ -119,8 +120,6 @@ my $_debug = 0;
 # Preloaded methods go here.
 
 ###############################################################################
-
-
 
 =head2 _do_query_generateScoreMatrixFromMetaAlignment
 
@@ -309,12 +308,188 @@ sub _do_query_generateScoreMatrixFromMetaAlignment {
     return ($MOBY_RESPONSE, $moby_exceptions);
 }
 
+sub _do_query_MemeMotifMatrices {
+    # $queryInput_DOM es un objeto DOM::Node con la informacion de una query biomoby
+    my $queryInput_DOM = shift @_;
+    my $_output_format = shift @_;
 
+    my $MOBY_RESPONSE   = "";     # set empty response
+    my $moby_exceptions = [];
+    my $output_article_name = "motif_weight_matrices";
+    
+    # Aqui escribimos las variables que necesitamos para la funcion.
+    my $matrix_mode;
+
+    # Variables that will be passed to meme2matrix_call
+    my $meme_predictions;
+    my %parameters;
+
+    my $queryID  = getInputID ($queryInput_DOM);
+    my @articles = getArticles($queryInput_DOM);
+
+    # Get the parameters
+
+    ($matrix_mode) = getNodeContentWithArticle($queryInput_DOM, "Parameter", "matrix mode");
+    if (not defined $matrix_mode) {
+	$matrix_mode = "log-likelihood";
+    }
+    elsif (! ((lc ($matrix_mode) eq "raw format") || (lc ($matrix_mode) eq "log-likelihood"))) {
+	my $note = "matrix mode parameter, '$matrix_mode', not accepted, should be ['raw format','log-likelihood']";
+	print STDERR "$note\n";
+	my $code = "222";
+	my $moby_exception = INB::Exceptions::MobyException->new (
+								  refElement => "matrix mode",
+								  code       => $code,
+								  type       => 'error',
+								  queryID    => $queryID,
+								  message    => "$note",
+								  );
+	push (@$moby_exceptions, $moby_exception);
+	
+	# Return an empty moby data object, as well as an exception telling why nothing got returned
+	
+	$MOBY_RESPONSE = INB::GRIB::Utils::CommonUtilsSubs->MOBY_EMPTY_RESPONSE ($queryID, $output_article_name);
+	return ($MOBY_RESPONSE, $moby_exceptions);
+    }
+    
+
+    # Match the matrix object name and the type of its element depending on matrix mode, ie
+    # raw format     => MatrixInteger
+    # log-likelihood => MatrixFloat
+
+    my $matrix_object_name;
+    my $matrix_element_type;
+    if ($matrix_mode eq "log-likelihood") {
+	$matrix_object_name  = "MatrixFloat";
+	$matrix_element_type = "Float";
+    }
+    elsif ($matrix_mode eq "raw format") {
+	$matrix_object_name  = "MatrixInteger";
+	$matrix_element_type = "Integer";
+    }
+    
+    # Add the parsed parameters in a hash table
+    
+    $parameters{matrix_mode} = $matrix_mode;
+
+    if ($_debug) {
+	print STDERR "matrix mode, $matrix_mode\n";
+    }
+
+    # Tratamos a cada uno de los articulos
+    foreach my $article (@articles) {
+
+	# El articulo es una tupla que contiene el nombre de este
+	# y su texto xml.
+
+	my ($articleName, $DOM) = @{$article}; # get the named article
+
+	# Si le hemos puesto nombre a los articulos del servicio,
+	# podemos recoger a traves de estos nombres el valor.
+	# Sino sabemos que es el input articulo porque es un simple/collection articulo
+
+	# It's not very nice but taverna doesn't set up easily article name for input data so we let the users not setting up the article name of the input (which should be 'sequences')
+	# In case of MatScan, it doesn't really matter as there is only one input anyway
+
+	if (($articleName eq "meme_predictions") || (isSimpleArticle ($DOM))) {
+
+	    if ($_debug) {
+		print STDERR "meme_predictions...\n";
+		print STDERR "Simple article DOM: " . $DOM->toString() . "\n";
+	    }
+	    
+	    if (isCollectionArticle ($DOM)) {
+		my $note = "Received a collection input article instead of a simple one";
+		print STDERR "$note\n";
+		my $code = "201";
+		my $moby_exception = INB::Exceptions::MobyException->new (
+									  refElement => "meme_predictions",
+									  code       => $code,
+									  type       => 'error',
+									  queryID    => $queryID,
+									  message    => "$note",
+									  );
+		push (@$moby_exceptions, $moby_exception);
+		
+		# Return an empty moby data object, as well as an exception telling what nothing got returned
+		
+		$MOBY_RESPONSE = INB::GRIB::Utils::CommonUtilsSubs->MOBY_EMPTY_RESPONSE ($queryID, $output_article_name);
+		return ($MOBY_RESPONSE, $moby_exceptions);
+	    }
+
+	    # Validation ...
+
+	    # ...
+	    
+	    $meme_predictions = INB::GRIB::Utils::CommonUtilsSubs->getTextContentFromXML ($DOM, "MEME_Text");
+
+	    if ($_debug) {
+		print STDERR "parsed meme_predictions,\n$meme_predictions.\n";
+	    }
+
+	}
+	if (isCollectionArticle ($DOM)) {
+	    my $note = "Received a collection input article instead of a simple one";
+	    print STDERR "$note\n";
+	    my $code = "201";
+	    my $moby_exception = INB::Exceptions::MobyException->new (
+								      refElement => "meme_predictions",
+								      code       => $code,
+								      type       => 'error',
+								      queryID    => $queryID,
+								      message    => "$note",
+								      );
+	    push (@$moby_exceptions, $moby_exception);
+	    
+	    # Return an empty moby data object, as well as an exception telling what nothing got returned
+	    
+	    $MOBY_RESPONSE = INB::GRIB::Utils::CommonUtilsSubs->MOBY_EMPTY_RESPONSE ($queryID, $output_article_name);
+	    return ($MOBY_RESPONSE, $moby_exceptions);
+	}
+    }
+
+    # Una vez recogido todos los parametros necesarios, llamamos a
+    # la funcion que nos devuelve el report.
+
+    # Return an array of matrices
+    my ($matrices_aref, $moby_exceptions_tmp) = meme2matrix_call (meme_predictions => $meme_predictions, format => $_output_format, queryID => $queryID, parameters => \%parameters);
+    push (@$moby_exceptions, @$moby_exceptions_tmp);
+    
+    # Ahora que tenemos la salida en el formato de la aplicacion XXXXXXX
+    # nos queda encapsularla en un Objeto bioMoby. Esta operacio
+    # la podriamos realizar en una funcion a parte si fuese compleja.
+
+    my $output_object_type  = "$_output_format";
+    my $namespace = "";
+    
+    if (not defined $matrices_aref) {
+	# Return an emtpy message !
+	$MOBY_RESPONSE = INB::GRIB::Utils::CommonUtilsSubs->MOBY_EMPTY_COLLECTION_RESPONSE ($queryID, $output_article_name);
+	return ($MOBY_RESPONSE, $moby_exceptions);
+    }
+    
+    my $meme_matrix_objects = [];    
+    foreach my $matrix (@$matrices_aref) {
+	my $moby_matrix = INB::GRIB::Utils::CommonUtilsSubs->convert_tabularPositionWeightMatrix_into_MobyMatrix ($matrix, $matrix_object_name, $matrix_element_type);
+	push (@$meme_matrix_objects, $moby_matrix);
+    }
+
+    # Bien!!! ya tenemos el objeto de salida del servicio , solo nos queda
+    # volver a encapsularlo en un objeto biomoby de respuesta. Pero
+    # en este caso disponemos de una funcion que lo realiza. Si tuvieramos
+    # una respuesta compleja (de verdad, esta era simple ;) llamariamos
+    # a collection response.
+    # IMPORTANTE: el identificador de la respuesta ($queryID) debe ser
+    # el mismo que el de la query.
+    
+    $MOBY_RESPONSE = collectionResponse($meme_matrix_objects, $output_article_name, $queryID);
+    return ($MOBY_RESPONSE, $moby_exceptions);
+    
+}
 
 #################################
 # Interface service methods
 #################################
-
 
 =head2 fromMetaAlignmentstoScoreMatrix
 
@@ -411,6 +586,90 @@ sub fromMetaAlignmentsToScoreMatrix {
     my $response = INB::GRIB::Utils::CommonUtilsSubs->setMobyResponse ($MOBY_RESPONSE, $moby_exceptions, $moby_logger, $serviceName);
     
     return $response;
+}
+
+
+=head2 parseMotifMatricesFromMEME
+
+ Title   : parseMotifMatricesFromMEME
+ Usage   : Esta función está pensada para llamarla desde un cliente SOAP.
+	 : No obstante, se recomienda probarla en la misma máquina, antes
+	 : de instalar el servicio. Para ello, podemos llamarla de la
+	 : siguiente forma:
+	 :
+	 : my $result = MatScan("call", $in);
+	 :
+	 : donde $in es texto que con el mensaje biomoby que contendría
+	 : la parte del <tag> "BODY" del mensaje soap. Es decir, un string
+	 : de la forma:
+	 :
+	 :  <?xml version='1.0' encoding='UTF-8'?>
+	 :   <moby:MOBY xmlns:moby='http://www.biomoby.org/moby-s'>
+	 :    <moby:mobyContent>
+	 :      <moby:mobyData queryID='1'>
+	 :      ...
+	 :      </moby:mobyData>
+	 :    </moby:mobyContent>
+	 :   </moby:mobyContent>
+	 :  </moby:MOBY>
+ Returns : Return a collection of text-formatted objects with MEME motif matrices
+
+=cut
+
+sub parseMotifMatricesFromMEME {
+
+    # El parametro $message es un texto xml con la peticion.
+    my ($caller, $message) = @_;        # get the incoming MOBY query XML
+
+    # Hasta el momento, no existen objetos Perl de BioMoby paralelos
+    # a la ontologia, y debemos contentarnos con trabajar directamente
+    # con objetos DOM. Por consiguiente lo primero es recolectar la
+    # lista de peticiones (queries) que tiene la peticion.
+    #
+    # En una misma llamada podemos tener mas de una peticion, y de
+    # cada servicio depende la forma de trabajar con ellas. En este
+    # caso las trataremos una a una, pero podriamos hacer Threads para
+    # tratarlas en paralelo, podemos ver si se pueden aprovechar resultados
+    # etc..
+    my @queries = getInputs($message);  # returns XML::DOM nodes
+    #
+    # Inicializamos la Respuesta a string vacio. Recordar que la respuesta
+    # es una coleccion de respuestas a cada una de las consultas.
+    my $MOBY_RESPONSE   = "";             # set empty response
+    my $moby_exceptions = [];
+    
+    my $_format = "MEME_Text";
+    my $moby_logger = get_logger ("MobyServices");
+    my $serviceName = "parseMotifMatricesFromMEME";
+    
+    # Para cada query ejecutaremos el _execute_query.
+    foreach my $queryInput (@queries){
+
+	# En este punto es importante recordar que el objeto $query
+	# es un XML::DOM::Node, y que si queremos trabajar con
+	# el mensaje de texto debemos llamar a: $query->toString()
+
+	if ($_debug) {
+	    my $query_str = $queryInput->toString();
+	    print STDERR "query text: $query_str\n";
+	}
+
+	my ($query_response, $moby_exceptions_tmp) = _do_query_MemeMotifMatrices ($queryInput, $_format);
+	push (@$moby_exceptions, @$moby_exceptions_tmp);
+	
+	# $query_response es un string que contiene el codigo xml de
+	# la respuesta.  Puesto que es un codigo bien formado, podemos
+	# encadenar sin problemas una respuesta con otra.
+	$MOBY_RESPONSE .= $query_response;
+    }
+    # Una vez tenemos la coleccion de respuestas, debemos encapsularlas
+    # todas ellas con una cabecera y un final. Esto lo podemos hacer
+    # con las llamadas de la libreria Common de BioMoby.
+    
+    my $response = INB::GRIB::Utils::CommonUtilsSubs->setMobyResponse ($MOBY_RESPONSE, $moby_exceptions, $moby_logger, $serviceName);
+    
+    return $response;
+
 }
 
 
