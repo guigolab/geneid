8144bf350483dceea370c2c903c7af21bd706557
Gmaster <gmaster@crg.eu>
2006-03-22 16:47:04 +0000

    Added implementation of fromMetaAlignmentsToTextScoreMatrix

--- projects/moby/lib/INB/GRIB/Services/ParsingServices.pm
+++ projects/moby/lib/INB/GRIB/Services/ParsingServices.pm
@@ -1,4 +1,4 @@
-# $Id: ParsingServices.pm,v 1.3 2006-03-22 10:15:15 gmaster Exp $
+# $Id: ParsingServices.pm,v 1.4 2006-03-22 16:47:04 gmaster Exp $
 #
 # This file is an instance of a template written 
 # by Roman Roset, INB (Instituto Nacional de Bioinformatica), Spain.
@@ -110,6 +110,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 # 
 our @EXPORT = qw(
   &fromMetaAlignmentstoScoreMatrix
+  &fromMetaAlignmentstoTextScoreMatrix
   &parseMotifMatricesFromMEME
 );
 
@@ -150,15 +151,16 @@ my $_debug = 0;
 
 sub _do_query_generateScoreMatrixFromMetaAlignment {
     # $queryInput_DOM es un objeto DOM::Node con la informacion de una query biomoby 
-    my $queryInput_DOM = shift @_;
-
+    my $queryInput_DOM      = shift @_;
+    my $output_format       = shift @_;
+    my $output_moby_type    = shift @_;
+    my $output_article_name = shift @_;
+    
     my $MOBY_RESPONSE   = "";     # set empty response
     my $moby_exceptions = [];
     
     # Aqui escribimos las variables que necesitamos para la funcion.
     my $input_format;
-    my $output_format = "Distance_Matrix";
-    my $output_article_name = "matrix";
     
     # Variables that will be passed to generateScoreMatrix_call
     
@@ -236,7 +238,7 @@ sub _do_query_generateScoreMatrixFromMetaAlignment {
 		$MOBY_RESPONSE = INB::GRIB::Utils::CommonUtilsSubs->MOBY_EMPTY_SIMPLE_RESPONSE ($queryID, $output_article_name);
 		return ($MOBY_RESPONSE, $moby_exceptions);
 	    }
-
+	    
 	    my @inputs_article_DOMs = getCollectedSimples ($DOM);
 	    foreach my $input_DOM (@inputs_article_DOMs) {
 		my $input = INB::GRIB::Utils::CommonUtilsSubs->getTextContentFromXML ($input_DOM, "Meta_Alignment_Text");
@@ -280,17 +282,46 @@ sub _do_query_generateScoreMatrixFromMetaAlignment {
     # nos queda encapsularla en un Objeto bioMoby. Esta operacio 
     # la podriamos realizar en una funcion a parte si fuese compleja.
     
+    if ($_debug) {
+	print STDERR "generated matrix:\n$matrix_text\n";
+    }
+    
     if (defined $matrix_text) {
 	
-	# Build the Moby Matrix object
+	my $output_object;
 	
-	my $output_object = INB::GRIB::Utils::CommonUtilsSubs->convert_tabularScoreMatrix_into_MobyMatrix ($matrix_text, "Distance_Matrix", "Float");
+	if ($output_format eq "xml") {
+	    
+	    if ($_debug) {
+		print STDERR "returning the matrix in xml format\n";
+	    }
+	    
+	    # Build the Moby Matrix object
+	    $output_object = INB::GRIB::Utils::CommonUtilsSubs->convert_tabularScoreMatrix_into_MobyMatrix ($matrix_text, $output_moby_type, "Float");
+	}
+	else {
+	    
+	    if ($_debug) {
+		print STDERR "returning the matrix in text format\n";
+	    }
+	    
+	    $output_object = <<PRT;
+<$output_moby_type namespace='$namespace' id=''>
+<String namespace='' id='' articleName='content'>
+<![CDATA[
+$matrix_text
+]]>
+</String>
+</$output_moby_type>
+PRT
+
+	}
 	
-        if ($_debug) {
+	if ($_debug) {
 	    print STDERR "Matrix moby object,\n";
 	    print STDERR "$output_object\n";
 	}
-
+	
         $MOBY_RESPONSE = simpleResponse($output_object, $output_article_name, $queryID);
     }
     else {
@@ -588,7 +619,112 @@ sub fromMetaAlignmentsToScoreMatrix {
 	    print STDERR "query text: $query_str\n";
 	}
 	
-	my ($query_response, $moby_exceptions_tmp) = _do_query_generateScoreMatrixFromMetaAlignment ($queryInput);
+	my $output_format       = "xml";
+	my $output_moby_type    = "Distance_Matrix";
+	my $output_article_name = "matrix";
+	
+	my ($query_response, $moby_exceptions_tmp) = _do_query_generateScoreMatrixFromMetaAlignment ($queryInput, $output_format, $output_moby_type, $output_article_name);
+	push (@$moby_exceptions, @$moby_exceptions_tmp);
+	
+	# $query_response es un string que contiene el codigo xml de
+	# la respuesta.  Puesto que es un codigo bien formado, podemos 
+	# encadenar sin problemas una respuesta con otra. 
+	$MOBY_RESPONSE .= $query_response;
+    }
+    # Una vez tenemos la coleccion de respuestas, debemos encapsularlas 
+    # todas ellas con una cabecera y un final. Esto lo podemos hacer 
+    # con las llamadas de la libreria Common de BioMoby. 
+    my $response = INB::GRIB::Utils::CommonUtilsSubs->setMobyResponse ($MOBY_RESPONSE, $moby_exceptions, $moby_logger, $serviceName);
+    
+    return $response;
+}
+
+=head2 fromMetaAlignmentstoTextScoreMatrix
+
+ Title   : fromMetaAlignmentstoTextScoreMatrix
+ Usage   : Esta función está pensada para llamarla desde un cliente SOAP. 
+         : No obstante, se recomienda probarla en la misma máquina, antes 
+         : de instalar el servicio. Para ello, podemos llamarla de la 
+         : siguiente forma:
+         : 
+         : my $result = GeneID("call", $in);
+         : 
+         : donde $in es texto que con el mensaje biomoby que contendría
+         : la parte del <tag> "BODY" del mensaje soap. Es decir, un string
+         : de la forma: 
+         :  
+         :  <?xml version='1.0' encoding='UTF-8'?>
+         :   <moby:MOBY xmlns:moby='http://www.biomoby.org/moby-s'>
+         :    <moby:mobyContent>
+         :      <moby:mobyData queryID='1'> 
+         :      ...
+         :      </moby:mobyData>
+         :    </moby:mobyContent>
+         :   </moby:mobyContent>
+         :  </moby:MOBY>
+ Returns : Devuelve un string que contiene el resultado de todas las 
+         : queries en GFF formate. Es decir, un mensaje xml de la forma:
+         :
+         : <?xml version='1.0' encoding='UTF-8'?>
+         : <moby:MOBY xmlns:moby='http://www.biomoby.org/moby' 
+         : xmlns='http://www.biomoby.org/moby'>
+         :   <moby:mobyContent moby:authority='inb.lsi.upc.es'>
+         :     <moby:mobyData moby:queryID='1'>
+         :       ....
+         :     </moby:mobyData>
+         :     <moby:mobyData moby:queryID='2'>
+         :       ....
+         :     </moby:mobyData>
+         :   </moby:mobyContent>
+         :</moby:MOBY>
+
+=cut
+
+sub fromMetaAlignmentsToTextScoreMatrix {
+    
+    # El parametro $message es un texto xml con la peticion.
+    my ($caller, $message) = @_; # get the incoming MOBY query XML
+
+    if ($_debug) {
+	print STDERR "processing Moby fromMetaAlignmentsToTextScoreMatrix query...\n";
+    }
+
+    # Hasta el momento, no existen objetos Perl de BioMoby paralelos 
+    # a la ontologia, y debemos contentarnos con trabajar directamente 
+    # con objetos DOM. Por consiguiente lo primero es recolectar la 
+    # lista de peticiones (queries) que tiene la peticion. 
+    # 
+    # En una misma llamada podemos tener mas de una peticion, y de  
+    # cada servicio depende la forma de trabajar con ellas. En este 
+    # caso las trataremos una a una, pero podriamos hacer Threads para 
+    # tratarlas en paralelo, podemos ver si se pueden aprovechar resultados 
+    # etc.. 
+    my @queries = getInputs($message);  # returns XML::DOM nodes
+    # 
+    # Inicializamos la Respuesta a string vacio. Recordar que la respuesta
+    # es una coleccion de respuestas a cada una de las consultas.
+    my $MOBY_RESPONSE   = "";             # set empty response
+    my $moby_exceptions = [];
+    my $moby_logger     = get_logger ("MobyServices");
+    my $serviceName     = "fromMetaAlignmentsToTextScoreMatrix";
+    
+    # Para cada query ejecutaremos el _execute_query.
+    foreach my $queryInput (@queries){
+	
+	# En este punto es importante recordar que el objeto $query 
+	# es un XML::DOM::Node, y que si queremos trabajar con 
+	# el mensaje de texto debemos llamar a: $query->toString() 
+	
+	if ($_debug) {
+	    my $query_str = $queryInput->toString();
+	    print STDERR "query text: $query_str\n";
+	}
+
+	my $output_format       = "text";
+	my $output_moby_type    = "MicroArrayData_Text";
+	my $output_article_name = "microarraydata";
+	
+	my ($query_response, $moby_exceptions_tmp) = _do_query_generateScoreMatrixFromMetaAlignment ($queryInput, $output_format, $output_moby_type, $output_article_name);
 	push (@$moby_exceptions, @$moby_exceptions_tmp);
 	
 	# $query_response es un string que contiene el codigo xml de
