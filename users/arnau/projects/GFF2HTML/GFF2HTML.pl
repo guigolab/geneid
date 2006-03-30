79e6711a6dce81e3c05a7e7631209c533eb3ec41
Arnau Bria <arnau.bria@crg.eu>
2006-03-30 13:33:36 +0000

    updated, works prettry good with genes (actually exons)

--- users/arnau/projects/GFF2HTML/GFF2HTML.pl
+++ users/arnau/projects/GFF2HTML/GFF2HTML.pl
@@ -8,7 +8,9 @@ use POSIX qw(ceil floor);
 my $_debug = 0;
 my @input_files = @ARGV;
 
-print STDERR "files:@input_files.\n";
+if ($_debug) {
+    print STDERR "files:@input_files.\n";
+}
 
 # could be 'Lowest p-value' if MEME
 my $score_type = "Score";
@@ -27,10 +29,14 @@ my %sequences;
 # parse GFF3 input file
 
 foreach my $input_file (@input_files) {
-    
-    print STDERR "parsing $input_file...\n";
+
+    if ($_debug) {
+	print STDERR "parsing $input_file...\n";
+    }
     
     my %features;
+    my @seq_starts = ();
+    my %feature_index_by_seq_starts;
     my $seq_id;
     
     open FILE, "<$input_file" or die "can't open file, $input_file!\n";
@@ -40,7 +46,9 @@ foreach my $input_file (@input_files) {
 	# parse the sequence length
 	
 	if ($line =~ /\# Sequence.region [^\s]+\s([^\s]+)\s(\d+)/) {
-	    print STDERR "parsing sequence length...\n";
+	    if ($_debug) {
+		print STDERR "parsing sequence length...\n";
+	    }
 	    $seq_start   = $1;
 	    $seq_end     = $2;
 	    $seq_length = $seq_end - $seq_start + 1;
@@ -62,7 +70,12 @@ foreach my $input_file (@input_files) {
 	    
 	    if ($method =~ /geneid/i) {
 		# keep only exon features
-		if ($feature_type ne "exon") {
+		if ($feature_type ne "CDS") {
+		    
+		    if ($_debug) {
+			print STDERR "filtering $feature_type\n";
+		    }
+		    
 		    next;
 		}
 	    }
@@ -75,6 +88,17 @@ foreach my $input_file (@input_files) {
 	    };
 	    
 	    $features{$index} = $feature;
+	    if (! is_in ($seq_start, @seq_starts)) {
+		push (@seq_starts, $seq_start);
+	    }
+	    if (defined $feature_index_by_seq_starts{$seq_start}) {
+		my $feature_indexes_tmp =  $feature_index_by_seq_starts{$seq_start};
+		push (@$feature_indexes_tmp, $index);
+		$feature_index_by_seq_starts{$seq_start} = $feature_indexes_tmp;
+	    }
+	    else {
+		$feature_index_by_seq_starts{$seq_start} = [$index];
+	    }
 	    
 	    $index++;
 	    
@@ -84,7 +108,9 @@ foreach my $input_file (@input_files) {
     close FILE;
     
     if (defined $seq_length) {
-	print STDERR "sequence length, $seq_length\n";
+	if ($_debug) {
+	    print STDERR "sequence length, $seq_length\n";
+	}
     }
     else {
 	print STDERR "no sequence length defined!\n";
@@ -92,22 +118,30 @@ foreach my $input_file (@input_files) {
     }
     
     my $nb_features = keys (%features);
-    print STDERR "nb features, $nb_features\n";
-
+    if ($_debug) {
+	print STDERR "nb features, $nb_features\n";
+    }
+    
+    # Sort the sequence starts
+    @seq_starts = sort {$a <=> $b} @seq_starts;
+    
     my $sequence = {
-	seq_id => $seq_id,
+	seq_id     => $seq_id,
 	seq_length => $seq_length,
-	features => \%features,
-	};
+	features   => \%features,
+	seq_starts => \@seq_starts,
+	feature_index_by_seq_starts => \%feature_index_by_seq_starts,
+    };
     
     $sequences{$seq_id} = $sequence;
     
 }
 
-# $gap = 25 * $seq_length / 500;
 $gap = floor ($seq_length / 20);
 
-print STDERR "gap, $gap\n";
+if ($_debug) {
+    print STDERR "gap, $gap\n";
+}
 
 # HTML generation
 
@@ -119,7 +153,7 @@ print "<TR><TH>Name<TH>Lowest<BR>$score_type<TH ALIGN=LEFT>&nbsp;&nbsp; Features
 
 my @seq_ids = keys ( %sequences);
 foreach my $seq_id (@seq_ids) {
-
+    
     my $sequence_href = $sequences{$seq_id};
     
     print "<TR>\n";
@@ -129,39 +163,63 @@ foreach my $seq_id (@seq_ids) {
     print "<TD>$seq_id\n";
     print "<TD ALIGN=RIGHT NOWRAP>$score\n";
     
-    my $features_href  = $sequence_href->{features};
-
+    my $features_href   = $sequence_href->{features};
+    my $feature_index_by_seq_starts_href = $sequence_href->{feature_index_by_seq_starts};
+    my $seq_starts_aref = $sequence_href->{seq_starts};
+    
     print "<TD>\n";
-
-    my $width = 1000 + 32;
+    
+    my $width = 1032;
+    my $length_feature = 1;
+    my $before_feature_width;
     
     print "<TABLE SUMMARY='diagram $seq_id' WIDTH=$width BORDER=0 ALIGN=LEFT CELLSPACING=0 CELLPADDING=0><TR ALIGN=CENTER>\n";
     if (keys %$features_href > 0) {
-
-	# in the meantime
-	# print "<TD WIDTH=$width><HR SIZE=4 NOSHADE>\n";
 	
-	my @feature_indexes = keys (%$features_href);
-	foreach my $feature_index (@feature_indexes) {
-	    my $feature = $features_href->{$feature_index};
-	    my $start   = $feature->{start};
-	    my $end     = $feature->{end};
-	    my $before_feature_width = $seq_start * 2;
-	    print "<TD WIDTH=$before_feature_width><HR SIZE=4 NOSHADE>\n";
-	    print "<TD CLASS='c0' WIDTH=30>+$feature_index\n";
+	my $start_reference = 1;
+	
+	# Loop over the features but in the order of position onto the sequence from 5' to 3' !!!!!!!!!!!
+	
+	foreach my $seq_start (@$seq_starts_aref) {
+	    my $feature_indexes_aref = $feature_index_by_seq_starts_href->{$seq_start};
 	    
-	    # temporary
-	    last;
+	    foreach my $feature_index (@$feature_indexes_aref) {
+		
+		my $feature = $features_href->{$feature_index};
+		my $start   = $feature->{start};
+		my $end     = $feature->{end};
+		$before_feature_width = floor (($start - $start_reference) * $width / $seq_length);
+		
+		if ($_debug) {
+		    print STDERR "feature index, $feature_index\n";
+		    print STDERR "exon start, $start\n";
+		    print STDERR "before_feature_width, $before_feature_width\n";
+		}
+		
+		print "<TD WIDTH=$before_feature_width><HR SIZE=4 NOSHADE>\n";
+		print "<TD CLASS='c0' WIDTH=$length_feature>+$feature_index\n";
+		
+		$start_reference = $start;
+	    }
+	    
+	    my $remaining = $width - $before_feature_width - $length_feature;
+	    
+	    if ($_debug) {
+		print STDERR "remaining, $remaining\n";
+	    }
+	    print "<TD WIDTH=$remaining><HR SIZE=4 NOSHADE>\n";
 	}
     }
     else {
 	print "<TD WIDTH=$width><HR SIZE=4 NOSHADE>\n";
     }
-
+    
     print "</TABLE>\n";
     print "<TR><TH CLASS='blue' COLSPAN=2 ROWSPAN=2 ALIGN=LEFT>SCALE\n";
     print "<TD><TABLE SUMMARY='scale' WIDTH=$width BORDER=0 ALIGN=LEFT CELLSPACING=0 CELLPADDING=0><TR ALIGN=CENTER>\n";
-
+    
+    # The scale
+    
     print "<TD CLASS='blue' WIDTH=$gap_width_adjusted ALIGN=LEFT>|</TD>\n";
     for (my $i =1; $i < 20; $i++) {
 	print "<TD CLASS='blue' WIDTH=$gap_width ALIGN=LEFT>|</TD>\n";
@@ -182,3 +240,16 @@ foreach my $seq_id (@seq_ids) {
 print "</TABLE>\n";
 
 print "</body>\n</html>\n";
+
+
+sub is_in {
+    my ($element, @elements) = @_;
+    
+    foreach my $element_test (@elements) {
+	if ($element_test == $element) {
+	    return 1;
+	}
+    }
+    
+    return 0;
+}
