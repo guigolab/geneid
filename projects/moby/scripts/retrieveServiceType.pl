aa6122741e67710c6f96230cefff35c5973e2db4
Arnau Bria <arnau.bria@crg.eu>
2006-04-28 09:52:50 +0000

    Added a script to retrieve info about a service type

---
+++ projects/moby/scripts/retrieveServiceType.pl
@@ -0,0 +1,134 @@
+#!/usr/local/bin/perl -w
+
+# Issue warnings about suspicious programming.
+use warnings 'all';
+
+# Must declare and initialize all variables
+use strict;
+use Data::Dumper;
+
+# Use the code module that contains MOBY Service.
+use MOBY::Client::Central;
+
+# be prepare for command-line options/arguments
+use Getopt::Std;
+
+sub help {
+return <<"END_HELP";
+Description: Retrieve a service type with its relationships from Moby Central
+Usage:
+
+retrieveServiceType.pl [-h] -x {Moby Central} -t {serviceType Name}
+	-h help
+	-x MOBY Central: Chirimoyo, Mobydev, Inab or BioMoby
+		<1> or Chirimoyo
+		<2> or mobydev
+		<3> or Inab
+		<4> or BioMoby
+	-t serviceType Name
+	
+Examples using some combinations:
+	perl retrieveServiceType.pl -x Inab -t serviceType
+
+END_HELP
+
+}
+
+
+BEGIN {
+	
+	# Determines the options with values from program
+	use vars qw/$opt_h $opt_x $opt_t/;
+	   
+	# these are switches taking an argument (a value)
+	my $switches = 'hxt';
+	   
+	# Get the switches
+	getopt($switches);
+	
+	# If the user does not write nothing, skip to help
+	if (defined($opt_h) || !defined($opt_x) || !defined($opt_t)){
+		print help;
+		exit 0;
+	}
+	
+}
+
+##############################################
+#	ASSIGN THE MOBY URI AND MOBY SERVER
+##############################################
+
+if (defined($opt_x)) {
+
+	# Delete spaces
+	$opt_x =~ s/\s//g;
+
+	# Assign the MOBY Server and MOBY URI
+	if (($opt_x == 1) || ($opt_x eq 'Chirimoyo')) {
+	
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$ENV{MOBY_URI}='http://chirimoyo.ac.uma.es/MOBY/Central';
+		$ENV{MOBY_SERVER}='http://chirimoyo.ac.uma.es/cgi-bin/MOBY-Central.pl';
+	
+	}elsif (($opt_x == 2) || ($opt_x eq 'Mobydev')) {
+	
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$ENV{MOBY_URI}='http://moby-dev.inab.org/MOBY/Central';
+		$ENV{MOBY_SERVER}='http://moby-dev.inab.org/cgi-bin/MOBY-Central.pl';
+
+	}elsif (($opt_x == 3) || ($opt_x eq 'Inab')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$ENV{MOBY_URI}='http://www.inab.org/MOBY/Central';
+		$ENV{MOBY_SERVER}='http://www.inab.org/cgi-bin/MOBY-Central.pl';
+	
+	}elsif (($opt_x == 4) || ($opt_x eq 'BioMoby')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$ENV{MOBY_URI}='http://mobycentral.icapture.ubc.ca/MOBY/Central';
+		$ENV{MOBY_SERVER}='http://mobycentral.icapture.ubc.ca/cgi-bin/MOBY05/mobycentral.pl';
+
+	}else {
+		print help;
+		exit 0;
+	}
+
+}else {
+	print help;
+	exit 0;
+}
+
+
+# Connect to MOBY-Central registries for searching.
+my $MOBYCentral = MOBY::Client::Central->new();
+
+if (! defined $MOBYCentral) {
+	print STDERR "Error, could not instanciate MOBY Central!\n";
+	exit 1;
+}
+
+my $serviceTypeName = $opt_t;
+
+# Retrieve the definition (type, description, relations, ...) from the input object.
+my $types = $MOBYCentral->retrieveServiceTypes();
+
+print "\n";
+
+if (! defined $types) {
+    print STDERR "Error, no defined service types!\n";
+}
+
+if (my $desc = $types->{$serviceTypeName}) {
+    print "definition for service type, $serviceTypeName, $desc\n";
+}
+else {
+    print "$serviceTypeName has no description!\n";
+}
+
+my $serviceTypeRelationships = $MOBYCentral->Relationships('serviceType' => $serviceTypeName);
+
+print STDERR "Dumping $serviceTypeRelationships, " . Dumper ($serviceTypeRelationships) . "\n";
