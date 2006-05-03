9d4cdd9ebce8c4a718be4cb1e05afb3597566aa1
Gmaster <gmaster@crg.eu>
2006-05-03 09:01:10 +0000

    RepeatMasker fixing, picks the masked sequence properly, None species handled properly

--- projects/moby/lib/INB/GRIB/Services/MaskingServices.pm
+++ projects/moby/lib/INB/GRIB/Services/MaskingServices.pm
@@ -1,4 +1,4 @@
-# $Id: MaskingServices.pm,v 1.4 2006-04-28 13:18:04 gmaster Exp $
+# $Id: MaskingServices.pm,v 1.5 2006-05-03 09:01:10 gmaster Exp $
 #
 # This file is an instance of a template written
 # by Roman Roset, INB (Instituto Nacional de Bioinformatica), Spain.
@@ -145,7 +145,7 @@ our @EXPORT = qw(
 
 our $VERSION = '1.0';
 
-my $_debug = 0;
+my $_debug = 1;
 
 # Preloaded methods go here.
 
