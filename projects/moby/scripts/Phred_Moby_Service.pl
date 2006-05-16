8e35de99f368358672e7fc30f92ae1c2d6d85b27
Arnau Bria <arnau.bria@crg.eu>
2006-05-16 16:32:42 +0000

    new script for testing phred

---
+++ projects/moby/scripts/Phred_Moby_Service.pl
@@ -0,0 +1,276 @@
+#!/usr/local/bin/perl -w
+
+##################################################################
+#
+# GeneID Moby Service Client
+#
+##################################################################
+
+use strict;
+use Data::Dumper;
+
+# be prepare for command-line options/arguments
+use Getopt::Std;
+
+# BioMoby and SOAP libraries
+
+use MOBY::Client::Central;
+use MOBY::Client::Service;
+# use SOAP::Lite + 'trace';
+
+# Benchmark Module
+use Benchmark;
+##################################################################
+
+sub help {
+return <<"END_HELP";
+Description: Execute Phred Moby services available from genome.imim.es
+Usage:
+
+    Phred_Moby_Service.pl [-h] -x {Moby Central} -s {Service Name} -f {chromatogram file}
+	-h help
+	-x MOBY Central: Chirimoyo, Mobydev, Inab or BioMoby
+		<1> or Chirimoyo
+		<2> or Mobydev
+		<3> or Inab
+		<4> or BioMoby
+	-s Service Name
+	-i Sequence(s) chromatogram - optional
+	
+Examples using some combinations:
+	perl Geneid_Moby_service.pl -x 4 -s runPhred -f /home/ug/arnau/data/Assembly_Galicia/ab1s/Clon1APlaca_G11-01PJZ.ab1
+
+END_HELP
+
+}
+
+BEGIN {
+	
+	# Determines the options with values from program
+	use vars qw/$opt_h $opt_x $opt_s $opt_f/;
+	   
+	# these are switches taking an argument (a value)
+	my $switches = 'hxsf';
+	   
+	# Get the switches
+	getopt($switches);
+	
+	# If the user does not write nothing, skip to help
+	if (defined($opt_h) || !defined($opt_x)){
+	    print help;
+	    exit 0;
+	}
+	
+}
+
+my $t1 = Benchmark->new ();
+
+my $_debug = 0;
+
+##################################################################
+#
+# Setup sequence input data and moby parameters
+#
+##################################################################
+
+my $serviceName = $opt_s || "runPhred";
+my $articleName = "trace";
+if ($serviceName =~ /collection/i) {
+    $articleName = "sequences";
+}
+$::authURI = 'genome.imim.es';
+
+my $in_file    = $opt_f || "/home/ug/arnau/data/Assembly_Galicia/ab1s/Clon1APlaca_G11-01PJZ.ab1";
+my $datasource = "";
+
+if (! -f $in_file) {
+    die "can't find file, $in_file!\n";
+}
+
+# No Parameters!
+
+
+
+##################################################################
+#
+# Setup Moby configuration parameters
+#
+##################################################################
+
+##############################################
+#
+# ASSIGN THE MOBY URI AND MOBY SERVER
+#
+##############################################
+
+my $URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://chirimoyo.ac.uma.es/cgi-bin/MOBY-Central.pl';
+my $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
+my $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+if (defined($opt_x)) {
+
+	# Delete spaces
+	$opt_x =~ s/\s//g;
+
+	# Assign the MOBY Server and MOBY URI
+	if (($opt_x == 1) || ($opt_x eq 'Chirimoyo')) {
+	    
+	    # export MOBY_URI
+	    # export MOBY_SERVER
+	    $URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://chirimoyo.ac.uma.es/cgi-bin/MOBY-Central.pl';
+	    $URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://chirimoyo.ac.uma.es/MOBY/Central';
+	    $PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+	
+	}elsif (($opt_x == 2) || ($opt_x eq 'Mobydev')) {
+	
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://moby-dev.inab.org/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://moby-dev.inab.org/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+	}elsif (($opt_x == 3) || ($opt_x eq 'Inab')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://www.inab.org/cgi-bin/MOBY-Central.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://www.inab.org/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+	
+	}elsif (($opt_x == 4) || ($opt_x eq 'BioMoby')) {
+
+		# export MOBY_URI
+		# export MOBY_SERVER
+		$URL   = $ENV{MOBY_SERVER}?$ENV{MOBY_SERVER}:'http://mobycentral.icapture.ubc.ca/cgi-bin/MOBY05/mobycentral.pl';
+		$URI   = $ENV{MOBY_URI}?$ENV{MOBY_URI}:'http://mobycentral.icapture.ubc.ca/MOBY/Central';
+		$PROXY = $ENV{MOBY_PROXY}?$ENV{MOBY_PROXY}:'No Proxy Server';
+
+	}else {
+		print help;
+		exit 0;
+	}
+
+}else {
+	print help;
+	exit 0;
+}
+
+##################################################################
+#
+# Moby Server Instanciation
+#
+##################################################################
+
+if ($_debug) {
+    print STDERR "Instanciating Central object...\n";
+}
+
+my $C = MOBY::Client::Central->new(
+				   Registries => {mobycentral => {URL => $URL,URI => $URI}}
+				   );
+
+if (defined $C) {
+    if ($_debug) {
+	print STDERR "Instanciation done.\n";
+	print STDERR "TESTING MOBY CLIENT with\n\tURL: $URL\n\tURI: $URI\n\tProxy: $PROXY\n\n";
+    }
+}
+else {
+    print STDERR "Error, Central could not be instanciated!\n";
+    exit 0;
+}
+
+##################################################################
+#
+# Moby Service instantiation
+#
+##################################################################
+
+if ($_debug) {
+    print STDERR "finding service, $serviceName...\n";
+}
+
+my ($service_instances, $reg) = $C->findService (
+						 serviceName  => "$serviceName",
+						 authURI      => $::authURI
+						 );
+
+if ($_debug) {
+    print STDERR "Service instances references: " . @$service_instances . "\n";
+}
+
+if (@$service_instances == 0) {
+    print STDERR "Error, can't find any service called $serviceName!\n";
+    exit 0;
+}
+
+my $service_instance = $service_instances->[0];
+
+my $wsdl = $C->retrieveService($service_instance);
+
+if ($_debug) {
+    print STDERR "wsdl: $wsdl\n";
+}
+
+if (!$wsdl || ($wsdl !~ /\<definitions/)){
+    print STDERR "test \t\t[FAIL]\tWSDL was not retrieved\n\n";
+}
+
+my $Service = MOBY::Client::Service->new(service => $wsdl);
+
+# Object construction
+
+my $chromatogram_data = qx/cat $in_file/;
+my @words = split (/\./, $in_file);
+
+my $chromatogram_id = $words[0];
+@words = split (/\//, $chromatogram_id);
+$chromatogram_id = pop (@words);
+
+if (! defined $chromatogram_id) {
+    die "can't get the chromatogram identifier from parsing the file name!\n";
+}
+else {
+    print STDERR "chromatogram identifier is the following, $chromatogram_id\n";
+}
+
+my $input_xml = <<PRT;
+<Chromatogram_Encoded namespace="$datasource" id="$chromatogram_id">
+  <String namespace="" id=""  articleName="rawdata"><![CDATA[$chromatogram_data]]</String>
+</Chromatogram_Encoded>
+PRT
+
+#
+# Parameters (secondary articles)
+#
+    
+  
+    
+##################################################################
+#
+# Service execution
+#
+##################################################################
+    
+my $result;
+if ($serviceName =~ /collection/i) {
+    $result = $Service->execute(XMLinputlist => [
+						 ["$articleName", [$input_xml]]
+						 ]);
+}
+else {
+    $result = $Service->execute(XMLinputlist => [
+						 ["$articleName", $input_xml]
+						 ]);
+}
+
+##################################################################
+#
+# Result processing
+#
+##################################################################
+
+print "result\n", $result, "\n";
+
+my $t2 = Benchmark->new ();
+print STDERR "\nTotal : ", timestr (timediff ($t2, $t1)), "\n";
