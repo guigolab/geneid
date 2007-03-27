9524c5eb058ef3e56ea3ac0266ed2f40133ef556
Gmaster <gmaster@crg.eu>
2007-03-27 18:59:28 +0000

    make it more generic by not hardcoding gmaster path

--- projects/moby/cgi-bin/config.pl
+++ projects/moby/cgi-bin/config.pl
@@ -1,33 +1,37 @@
 # Production Moby GRIB libraries location
-use lib "/home/ug/gmaster/projects/moby/prod/lib";
+use lib "$HOME/projects/moby/prod/lib";
+
+# print STDERR "HOME: $HOME\n";
 
 # BioMoby core libraries
-use lib "/home/ug/gmaster/projects/moby/biomoby.0.8.2a/Perl";
-# use lib "/home/ug/gmaster/projects/moby/biomoby.perl.22.02.2006/Perl";
+use lib "$HOME/projects/moby/biomoby.0.8.2a/Perl";
+# use lib "$HOME/projects/moby/biomoby.perl.22.02.2006/Perl";
+# use lib "$HOME/projects/moby/Moby_Perl.07.02.2007/Perl";
+# use lib "$HOME/projects/moby/Moby_Perl.27.03.2007/Perl";
 
 # Also need SOAP and Bioperl libraries that are already installed in the default perl library path
 # /usr/local/Install/perl-5.8.5/lib/site_perl/5.8.5/Bio
 
-# SOAP v0.68
-# use lib "/home/ug/gmaster/projects/lib/5.8.5/i686-linux-thread-multi";
-# use lib "/home/ug/gmaster/projects/lib/site_perl/5.8.5";
+# SOAP v0.60
+use lib "$HOME/projects/lib/5.8.8/x86_64-linux-thread-multi";
+use lib "$HOME/projects/lib/site_perl/5.8.8";
 
 # Log::Log4perl and depedencies - temporary place !
-# Installed Globally now
 # Also CGI::Ajax HTML::Template Class::Accessor
-use lib "/home/ug/gmaster/lib/site_perl/5.8.5";
-use lib "/home/ug/gmaster/lib/site_perl/5.8.5/i686-linux-thread-multi";
+# Installed Globally now
+# use lib "$HOME/lib/site_perl/5.8.5";
+# use lib "$HOME/lib/site_perl/5.8.5/i686-linux-thread-multi";
 
 # GOstat setup
 
 # GOstat libraries
-use lib "/home/ug/gmaster/projects/gostat/lib";
-$ENV{GOSTAT} = "/home/ug/gmaster/projects/gostat";
+use lib "$HOME/projects/gostat/lib";
+$ENV{GOSTAT} = "$HOME/projects/gostat";
 
 # SGP2 setup
-$ENV{SGP2} = "/home/ug/gmaster/projects/sgp2";
+$ENV{SGP2} = "$HOME/projects/sgp2";
 
 # Statistic reporting file name
-$ENV{STATS_FILE}="/home/ug/gmaster/projects/moby_logs/moby_services_statistics.log";
+$ENV{STATS_FILE}="$HOME/projects/moby_logs/moby_services_statistics.log";
 
 1;
