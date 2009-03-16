c724571fad8a716a62e71ee78912093b2335fac9
Tyler Alioto <tyler.alioto@gmail.com>
2009-03-16 13:51:13 +0000

    changed filename length and infinity value

--- include/geneid.h
+++ include/geneid.h
@@ -28,7 +28,7 @@
 *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.             *
 *************************************************************************/     
 
-/* $Id: geneid.h,v 1.47 2009-03-16 13:40:25 talioto Exp $ */
+/* $Id: geneid.h,v 1.48 2009-03-16 13:51:13 talioto Exp $ */
 
 /* Required libraries */
 #include <stdlib.h>
@@ -192,7 +192,7 @@ A. DEFINITIONS
 #define HASHFACTOR 3             
 
 /* maximum length of filenames              */
-#define FILENAMELENGTH 500       
+#define FILENAMELENGTH 5000       
 
 /* Name of default parameter file           */
 #define PARAMETERFILE  "param.default"   
@@ -312,7 +312,7 @@ A. DEFINITIONS
 #define sENDRVS   "End-"
 
 /* Infinity: positions in sequence          */
-#define INFI 999999999           
+#define INFI 3999999999           
 
 /* Infinity: word in Gene model             */
 #define SINFI "Infinity"         
